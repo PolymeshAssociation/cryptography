2ce1feb962e5456997be77e239f8164245b227ae
Parnian Alimi <parnian@polymath.network>
2020-02-20 10:03:47 -0700

    Add project skeleton.

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+pub mod pedersen_commitments;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
---
+++ src/pedersen_commitments/mod.rs
@@ -0,0 +1,6 @@
+
+extern crate curve25519_dalek;
+use curve25519_dalek::*;
+
+extern crate bulletproofs;
+use bulletproofs::*;
