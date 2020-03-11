7aba291787814837a75df9c02bed06fa99a673e1
Parnian <60563484+ParnianAlimi@users.noreply.github.com>
2020-03-11 13:44:56 -0600

    CRYP-15: Add prover and verifier CLIs. (#7)
    Add demos for the `claim_proofs` library.
    
    * CRYP-15: Add prover and verifier CLIs.
    
    * Improve error handling.

--- cli/src/examples/simple.rs
+++
@@ -1,8 +0,0 @@
-use cryptography::pedersen_commitments::PedersenGenerators;
-
-fn main() {
-    println!("Hello, world!");
-
-    let plg = PedersenGenerators::default();
-    println!("{:?}", plg);
-}
