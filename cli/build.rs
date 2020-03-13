fcf95a2460ab7acaaf8a003e211e3458396fcbe8
miguel <francisco.miguel.garcia.rodriguez@gmail.com>
2020-03-13 16:21:29 +0100

    Add WASM build process

---
+++ cli/build.rs
@@ -0,0 +1,7 @@
+use wasm_builder_runner::{build_current_project, WasmBuilderSource};
+fn main() {
+    build_current_project(
+        "wwasm_binarpasm_binary.rs",
+        WasmBuilderSource::Crates("1.0.8"),
+    );
+}
