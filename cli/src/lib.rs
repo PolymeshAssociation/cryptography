6e6030e68c7f7e7d24954ae43421c6128881b1d9
miguel <francisco.miguel.garcia.rodriguez@gmail.com>
2020-03-18 13:27:09 +0100

    Enable WASM build

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -1,8 +1,2 @@
-#![cfg_attr(not(feature = "std"), no_std)]
-#![feature(nll)]
-#![feature(external_doc)]
-#![feature(try_trait)]
-// #![deny(missing_docs)]
-
 pub mod claim_proofs;
 pub mod pedersen_commitments;
