b3a465295502207b2b6f7e13ca90a35fe18e7953
Parnian Alimi <parnian@polymath.network>
2020-04-19 20:30:04 -0600

    Add support for NIZKP of multiple properties. Add more documentation.

--- cli/cryptography/src/asset_proofs/encryption_proofs.rs
+++ cli/cryptography/src/asset_proofs/encryption_proofs.rs
@@ -1,32 +1,71 @@
+//! The `encryption_proofs` library contains API for generating
+//! and verifying proofs of various properties of an encrypted
+//! value proofs as part of the MERCAT
+//! (Mediated, Encrypted, Reversible, SeCure Asset Transfers)
+//! Project.
+//!
+//! For a full description of these proofs see section 5 of the
+//! whitepaper. [todo: Add a link to the whitepaper.]
+
+use crate::asset_proofs::AssetProofError;
 use bulletproofs::PedersenGens;
 use curve25519_dalek::{ristretto::CompressedRistretto, scalar::Scalar};
 use merlin::Transcript;
 use rand_core::{CryptoRng, RngCore};
 
-const ENCRYPTION_PROOF_LABEL: &[u8] = b"PolymathEncryptionProof";
+/// The domain label for the encryption proofs.
+pub const ENCRYPTION_PROOFS_LABEL: &[u8] = b"PolymathEncryptionProofs";
+/// The domain label for the challenge.
+pub const ENCRYPTION_PROOFS_CHALLENGE_LABEL: &[u8] = b"PolymathEncryptionProofsChallenge";
 
-// todo better names
-pub struct ZKChallenge {
-    pub x: Scalar,
-}
+// ------------------------------------------------------------------------
+// Dealer Implementation
+// ------------------------------------------------------------------------
 
+/// The role of a dealer in a non-interactive zero knowledge proof system
+/// is to provide a challenge without revealing any information about the
+/// secrets while protecting against Chosen Message attacks.
+///
+/// ZKDealer implements the Fiat-Shamir huristic via Merlin transcripts.
 pub struct ZKDealer {
     transcript: Transcript,
 }
 
 impl ZKDealer {
-    // pub fn new(transcript: &'a mut Transcript) -> Self {
-    pub fn new() -> Self {
-        let t = Transcript::new(&ENCRYPTION_PROOF_LABEL);
+    pub fn new(label: &'static [u8]) -> Self {
+        let t = Transcript::new(label);
         ZKDealer { transcript: t }
     }
 
-    pub fn dealer_append_point(&mut self, label: &'static [u8], m: &CompressedRistretto) {
-        // todo this could potentially do a check and return error
-        // this could take in a label as well
-        self.transcript.append_message(label, m.as_bytes());
+    /// If the inputted message is not trivial append it to the Dealer's
+    /// state.
+    /// # Inputs
+    /// * `label`   a domain label for the point to append.
+    /// * `message` a compressed Ristretto point.
+    ///
+    /// # Output
+    /// Ok on success, or an error on failure.
+    pub fn dealer_append_validated_point(
+        &mut self,
+        label: &'static [u8],
+        message: &CompressedRistretto,
+    ) -> Result<(), AssetProofError> {
+        use curve25519_dalek::traits::IsIdentity;
+        if message.is_identity() {
+            // todo test the error case
+            Err(AssetProofError::VerificationError)
+        } else {
+            Ok(self.transcript.append_message(label, message.as_bytes()))
+        }
     }
 
+    /// Get the protocol's challenge.
+    ///
+    /// # Inputs
+    /// * `label` a domain label.
+    ///
+    /// # Output
+    /// A scalar challenge.
     pub fn dealer_scalar_challenge(&mut self, label: &'static [u8]) -> ZKChallenge {
         let mut buf = [0u8; 64];
         self.transcript.challenge_bytes(label, &mut buf);
@@ -37,70 +76,205 @@ impl ZKDealer {
     }
 }
 
+/// A trait that is used to update the dealer with the partial proof
+/// that results from the first round of the protocol.
 pub trait UpdateZKDealer {
-    fn update_dealer(&self, d: &mut ZKDealer);
+    fn update_dealer(&self, d: &mut ZKDealer) -> Result<(), AssetProofError>;
 }
 
-/// The interface for 3-Sigma proofs of properties of an encrypted value.
+// ------------------------------------------------------------------------
+// Sigma Protocol's Prover and Verifier Interfaces
+// ------------------------------------------------------------------------
+
+// todo better names?
+/// A scalar challenge.
+pub struct ZKChallenge {
+    pub x: Scalar,
+}
+
+/// The interface for a 3-Sigma protocol.
+/// Abstracting the prover and verifier roles.
 ///
-// pub trait AssetProofProver<ZKPartialProof: UpdateZKDealer, ZKProof> {
+/// Each proof needs to use the same `ZKPartialProof` and `ZKProof` types.
+/// Each `ZKPartialProof` needs to implement the `UpdateZKDealer` trait.
 pub trait AssetProofProver {
     type ZKPartialProof: UpdateZKDealer;
     type ZKProof;
+
+    /// First round of the Sigma protocol. Prover generates a partial proof.
+    ///
+    /// # Inputs
+    /// `pc_gens` The Pedersen Generators used for the Elgamal encryption.
+    /// `rng`     An RNG.
+    ///
+    /// # Output
+    /// A partial proof.
     fn prover_generate_partial_proof<T: RngCore + CryptoRng>(
         &mut self,
         pc_gens: &PedersenGens,
         rng: &mut T,
     ) -> Self::ZKPartialProof;
 
-    fn prover_apply_challenge(&self, c: &ZKChallenge) -> Self::ZKProof;
+    /// Third round of the Sigma protocol. Prover receives a challenge and
+    /// uses it to generate the proof.
+    ///
+    /// # Inputs
+    /// `challenge` The scalar challenge, generated by the Dealer.
+    ///
+    /// # Output
+    /// A proof.
+    fn prover_apply_challenge(&self, challenge: &ZKChallenge) -> Self::ZKProof;
 }
 
-// pub trait AssetProofVerifier<ZKPartialProof: UpdateZKDealer, ZKProof> {
 pub trait AssetProofVerifier {
     type ZKPartialProof: UpdateZKDealer;
     type ZKProof;
 
+    /// Forth round of the Sigma protocol. Verifier receives the partial proof
+    /// and the proof, and verifies them.
+    ///
+    /// # Inputs
+    /// `pc_gens`       The Pedersen Generators used for the Elgamal encryption.
+    /// `challenge`     The scalar challenge, generated by the Dealer.
+    /// `partial_proof` The partial proof, generated by the Prover.
+    /// `proof`         The proof, generated by the Prover.
+    ///
+    /// # Output
+    /// Ok on success, or an error on failure.
     fn verifier_verify(
         &self,
         pc_gens: &PedersenGens,
         challenge: &ZKChallenge,
-        pp: &Self::ZKPartialProof,
-        z: &Self::ZKProof,
-    ) -> bool;
+        partial_proof: &Self::ZKPartialProof,
+        proof: &Self::ZKProof,
+    ) -> Result<(), AssetProofError>;
 }
 
+// ------------------------------------------------------------------------
+// Non-Interactive Zero Knowledge Proofs API
+// ------------------------------------------------------------------------
+
+/// The non-interactive implementation of the protocol for a single
+/// encryption proof's prover role.
+///
+/// # Inputs
+/// `prover` Any prover that implements the `AssetProofProver` trait.
+/// `rng`    An RNG.
+///
+/// # Outputs
+/// A partial proof and a proof on success, or failure on an error.
 pub fn single_property_prover<T: RngCore + CryptoRng, Prover: AssetProofProver>(
     prover: &mut Prover,
     rng: &mut T,
-) -> (Prover::ZKPartialProof, Prover::ZKProof) {
-    // todo use a different label.
-    // let mut t = Transcript::new(ENCRYPTION_PROOF_LABEL);
-    let mut dealer = ZKDealer::new();
+) -> Result<(Prover::ZKPartialProof, Prover::ZKProof), AssetProofError> {
+    let mut dealer = ZKDealer::new(ENCRYPTION_PROOFS_LABEL);
     let gens = PedersenGens::default();
     let message = prover.prover_generate_partial_proof(&gens, rng);
 
-    message.update_dealer(&mut dealer);
+    message.update_dealer(&mut dealer)?;
 
-    let challenge = dealer.dealer_scalar_challenge(b"finalize");
+    let challenge = dealer.dealer_scalar_challenge(ENCRYPTION_PROOFS_CHALLENGE_LABEL);
 
-    (message, prover.prover_apply_challenge(&challenge))
+    Ok((message, prover.prover_apply_challenge(&challenge)))
+    // let (partial_proofs, proofs) = prove_multiple_encryption_properties(&mut vec![prover], rng)?;
+    // Ok((partial_proofs[0], proofs[0]))
 }
 
+/// The non-interactive implementation of the protocol for a single
+/// encryption proof's verifier role.
+///
+/// # Inputs
+/// `verifier` Any verifier that implements the `AssetProofVerifier` trait.
+/// `rng`      An RNG.
+///
+/// # Outputs
+/// Ok on success, or failure on error.
 pub fn single_property_verifier<Verifier: AssetProofVerifier>(
     verifier: &Verifier,
-    pp: &Verifier::ZKPartialProof,
-    p: &Verifier::ZKProof,
-) -> bool {
+    partial_proof: &Verifier::ZKPartialProof,
+    proof: &Verifier::ZKProof,
+) -> Result<(), AssetProofError> {
+    verify_multiple_encryption_properties(&[verifier], (&[partial_proof], &[proof]))
+}
+
+/// The non-interactive implementation of the protocol for multiple provers
+/// which use the same challenge. In this scenario the Dealer combines all
+/// the partial proofs to generate a single challenge.
+///
+/// # Inputs
+/// `provers` An array of provers that implement the `AssetProofProver` trait.
+/// `rng`     An RNG.
+///
+/// # Outputs
+/// An array of partial proofs and proofs on success, or failure on error.
+pub fn prove_multiple_encryption_properties<T: RngCore + CryptoRng, Prover: AssetProofProver>(
+    provers: &mut Vec<Prover>,
+    rng: &mut T,
+) -> Result<(Vec<Prover::ZKPartialProof>, Vec<Prover::ZKProof>), AssetProofError> {
     // todo use a different label.
-    // let mut t = Transcript::new(ENCRYPTION_PROOF_LABEL);
-    let mut dealer = ZKDealer::new();
+    let mut dealer = ZKDealer::new(ENCRYPTION_PROOFS_LABEL);
     let gens = PedersenGens::default();
 
-    pp.update_dealer(&mut dealer);
-    let challenge = dealer.dealer_scalar_challenge(b"finalize");
-    verifier.verifier_verify(&gens, &challenge, pp, p)
+    // ToDo is it safe to pass the rng between provers like this? will this code get parallelized?
+    let partial_proofs: Vec<_> = provers
+        .iter_mut()
+        .map(|prover| prover.prover_generate_partial_proof(&gens, rng))
+        .collect();
+
+    // Combine all the partial proofs to create a single challenge.
+    partial_proofs
+        .iter()
+        .map(|partial_proof| partial_proof.update_dealer(&mut dealer))
+        .collect::<Result<(), _>>()?;
+
+    let challenge = dealer.dealer_scalar_challenge(ENCRYPTION_PROOFS_CHALLENGE_LABEL);
+
+    let proofs: Vec<_> = provers
+        .iter()
+        .map(|p| p.prover_apply_challenge(&challenge))
+        .collect();
+
+    Ok((partial_proofs, proofs))
 }
+
+/// The non-interactive implementation of the protocol for multiple verifiers
+/// which use the same challenge. In this scenario the Dealer combines all
+/// the partial proofs to generate a single challenge.
+///
+/// # Inputs
+/// `verifiers` An array of verifiers that implement the `AssetProofVerifier` trait.
+/// `rng`       An RNG.
+///
+/// # Outputs
+/// Ok on success, or failure on error.
+pub fn verify_multiple_encryption_properties<Verifier: AssetProofVerifier>(
+    verifiers: &[&Verifier],
+    (partial_proofs, proofs): (&[&Verifier::ZKPartialProof], &[&Verifier::ZKProof]),
+) -> Result<(), AssetProofError> {
+    if partial_proofs.len() != proofs.len() || verifiers.len() != proofs.len() {
+        return Err(AssetProofError::VerificationError);
+    }
+
+    let mut dealer = ZKDealer::new(ENCRYPTION_PROOFS_LABEL);
+    let gens = PedersenGens::default();
+
+    // Combine all the partial proofs to create a single challenge.
+    partial_proofs
+        .iter()
+        .map(|partial_proof| partial_proof.update_dealer(&mut dealer))
+        .collect::<Result<(), _>>()?;
+
+    let challenge = dealer.dealer_scalar_challenge(ENCRYPTION_PROOFS_CHALLENGE_LABEL);
+    for i in 0..verifiers.len() {
+        verifiers[i].verifier_verify(&gens, &challenge, &partial_proofs[i], &proofs[i])?;
+    }
+
+    Ok(())
+}
+
+// ------------------------------------------------------------------------
+// Tests
+// ------------------------------------------------------------------------
 
 #[cfg(test)]
 mod tests {
@@ -128,14 +302,10 @@ mod tests {
         let cipher = elg_pub.encrypt(&w);
 
         let mut prover = CorrectnessProver::new(&elg_pub, &w);
-        let mut verifier = CorrectnessVerifier::new(&secret_value, &elg_pub, &cipher);
+        let verifier = CorrectnessVerifier::new(&secret_value, &elg_pub, &cipher);
 
-        let (partial_proof, proof) = single_property_prover(&mut prover, &mut rng);
+        let (partial_proof, proof) = single_property_prover(&mut prover, &mut rng).unwrap();
 
-        assert!(single_property_verifier(
-            &mut verifier,
-            &partial_proof,
-            &proof
-        ));
+        assert!(single_property_verifier(&verifier, &partial_proof, &proof).is_ok());
     }
 }
