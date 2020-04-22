f3bf36c75ce399c4de64588e0ea4749a008315af
Parnian Alimi <parnian@polymath.network>
2020-04-22 17:00:53 -0600

    Address all review comments: * use boxes around generic types in `prove_multiple_encryption_properties()`. * replace the Dealer with a merlin transcript that implements our TranscriptProtocol. * use proper error checking in unit tests. * use the `failure` crate for error handling.

---
+++ cryptography/src/asset_proofs/transcript.rs
@@ -0,0 +1,93 @@
+//! The TranscriptProtocol implementation for a Merlin transcript.
+//!
+//! The role of a Merlin transcript in a non-interactive zero knowledge
+//! proof system is to provide a challenge without revealing any information
+//! about the secrets while protecting against Chosen Message attacks.
+
+use crate::asset_proofs::encryption_proofs::ZKPChallenge;
+use crate::asset_proofs::errors::AssetProofError;
+use curve25519_dalek::{ristretto::CompressedRistretto, scalar::Scalar};
+use merlin::Transcript;
+
+pub trait TranscriptProtocol {
+    /// If the inputted message is not trivial append it to the
+    /// transcript's state.
+    ///
+    /// # Inputs
+    /// * `label`   a domain label for the point to append.
+    /// * `message` a compressed Ristretto point.
+    ///
+    /// # Output
+    /// Ok on success, or an error on failure.
+    fn append_validated_point(
+        &mut self,
+        label: &'static [u8],
+        message: &CompressedRistretto,
+    ) -> Result<(), AssetProofError>;
+
+    /// Appends a domain separator string to the transcript's state.
+    ///
+    /// # Inputs
+    /// * `message` a message string.
+    fn append_domain_separator(&mut self, message: &'static [u8]);
+
+    /// Get the protocol's challenge.
+    ///
+    /// # Inputs
+    /// * `label` a domain label.
+    ///
+    /// # Output
+    /// A scalar challenge.
+    fn scalar_challenge(&mut self, label: &'static [u8]) -> ZKPChallenge;
+}
+
+impl TranscriptProtocol for Transcript {
+    fn append_validated_point(
+        &mut self,
+        label: &'static [u8],
+        message: &CompressedRistretto,
+    ) -> Result<(), AssetProofError> {
+        use curve25519_dalek::traits::IsIdentity;
+
+        if message.is_identity() {
+            // println!("validation fails! point: {:?}", message);
+            Err(AssetProofError::VerificationError)
+        } else {
+            Ok(self.append_message(label, message.as_bytes()))
+        }
+    }
+
+    fn append_domain_separator(&mut self, message: &'static [u8]) {
+        self.append_message(b"dom-sep", message)
+    }
+
+    fn scalar_challenge(&mut self, label: &'static [u8]) -> ZKPChallenge {
+        let mut buf = [0u8; 64];
+        self.challenge_bytes(label, &mut buf);
+
+        ZKPChallenge {
+            x: Scalar::from_bytes_mod_order_wide(&buf),
+        }
+    }
+}
+
+/// A trait that is used to update the transcript with the partial proof
+/// that results from the first round of the protocol.
+pub trait UpdateTranscript {
+    fn update_transcript(&self, d: &mut Transcript) -> Result<(), AssetProofError>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn detect_trivial_message() {
+        use curve25519_dalek::ristretto::CompressedRistretto;
+        let mut transcript = Transcript::new(b"unit test");
+        assert_eq!(
+            transcript.append_validated_point(b"identity", &CompressedRistretto::default()),
+            Err(AssetProofError::VerificationError)
+        );
+    }
+}
--- cryptography/src/lib.rs
+++ cryptography/src/lib.rs
@@ -1,2 +1,5 @@
+#[macro_use]
+extern crate failure;
+
 pub mod asset_proofs;
 pub mod claim_proofs;
