ea355059a08b8ad6f4cbd52cb8f0e1f4c326d4e8
Arash Afshar <63250695+aafshar-poly@users.noreply.github.com>
2020-05-06 15:51:08 -0600

    confidential transaction and confidential asset issuance (#24)
    Addresses CRYP-69 (CRYP-73, CRYP-74, CRYP-75, CRYP-76) and  CRYP-60
    
    * Adds the public interface for the MERCAT protocol for confidential transactions,
      reversal of confidential transaction, and confidential asset issuance
    * Separates the signing keys from the encryption keys

---
+++ cryptography/src/mercat/errors.rs
@@ -0,0 +1,21 @@
+use failure::{Error, Fail};
+
+/// Represents an error in asset issuance transaction.
+/// TODO: as we implement the methods, we will find more explicit types for
+/// the error.
+#[derive(Fail, Clone, Debug, Eq, PartialEq)]
+pub enum AssetTXError {
+    #[fail(display = "CHANGEME as needed")]
+    ChangeMe,
+}
+
+/// Represents an error in confidential transaction.
+/// TODO: as we implement the methods, we will find more explicit types for
+/// the error.
+#[derive(Fail, Clone, Debug, Eq, PartialEq)]
+pub enum ConfidentialTXError {
+    #[fail(display = "CHANGEME as needed")]
+    ChangeMe,
+}
+
+pub type Result<T, E = Error> = std::result::Result<T, E>;
