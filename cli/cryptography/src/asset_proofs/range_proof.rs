fe50f6135685569a3eb0ed1f906d43b6d20bfaaa
Parnian <60563484+ParnianAlimi@users.noreply.github.com>
2020-05-19 13:32:43 -0600

    Add support for proof serialization. (#35)
    Address CRYP-78. Add support for proof serialization.

--- cli/cryptography/src/asset_proofs/range_proof.rs
+++ cli/cryptography/src/asset_proofs/range_proof.rs
@@ -7,12 +7,19 @@ use crate::asset_proofs::errors::{AssetProofError, Result};
 use bulletproofs::{BulletproofGens, PedersenGens, RangeProof};
 use curve25519_dalek::{ristretto::CompressedRistretto, scalar::Scalar};
 use merlin::Transcript;
+use serde::{Deserialize, Serialize};
 
 const RANGE_PROOF_LABEL: &[u8] = b"PolymathRangeProof";
 
 // ------------------------------------------------------------------------
 // Range Proof
 // ------------------------------------------------------------------------
+
+#[derive(Serialize, Deserialize, PartialEq, Copy, Clone, Debug)]
+pub struct RangeProofInitialMessage(CompressedRistretto);
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct RangeProofFinalResponse(RangeProof);
 
 /// Generate a range proof for a commitment to a secret value.
 /// Range proof commitments are equevalant to the second term (Y)
@@ -21,7 +28,7 @@ pub fn prove_within_range(
     secret_value: u64,
     rand_blind: Scalar,
     range: usize,
-) -> Result<(RangeProof, CompressedRistretto)> {
+) -> Result<(RangeProofInitialMessage, RangeProofFinalResponse)> {
     // Generators for Pedersen commitments.
     let pc_gens = PedersenGens::default();
 
@@ -35,7 +42,7 @@ pub fn prove_within_range(
     // the Fiat-Shamir huristic.
     let mut prover_transcript = Transcript::new(RANGE_PROOF_LABEL);
 
-    RangeProof::prove_single(
+    let (proof, commitment) = RangeProof::prove_single(
         &bp_gens,
         &pc_gens,
         &mut prover_transcript,
@@ -43,13 +50,18 @@ pub fn prove_within_range(
         &rand_blind,
         range,
     )
-    .map_err(|source| AssetProofError::ProvingError { source }.into())
+    .map_err(|source| AssetProofError::ProvingError { source })?;
+
+    Ok((
+        RangeProofInitialMessage(commitment),
+        RangeProofFinalResponse(proof),
+    ))
 }
 
 /// Verify that a range proof is valid given a commitment to a secret value.
 pub fn verify_within_range(
-    proof: RangeProof,
-    commitment: CompressedRistretto,
+    commitment: RangeProofInitialMessage,
+    proof: RangeProofFinalResponse,
     range: usize,
 ) -> bool {
     // Generators for Pedersen commitments.
@@ -64,11 +76,12 @@ pub fn verify_within_range(
     let mut verifier_transcript = Transcript::new(RANGE_PROOF_LABEL);
 
     proof
+        .0
         .verify_single(
             &bp_gens,
             &pc_gens,
             &mut verifier_transcript,
-            &commitment,
+            &commitment.0,
             range,
         )
         .is_ok()
@@ -83,6 +96,7 @@ mod tests {
     extern crate wasm_bindgen_test;
     use super::*;
     use crate::asset_proofs::*;
+    use bincode::{deserialize, serialize};
     use rand::{rngs::StdRng, SeedableRng};
     use std::convert::TryFrom;
     use wasm_bindgen_test::*;
@@ -97,21 +111,46 @@ mod tests {
         let secret_value = 42u32;
         let rand_blind = Scalar::random(&mut rng);
 
-        let (proof, initial_message) = prove_within_range(secret_value as u64, rand_blind, 32)
-            .expect("This shouldn't happen.");
-        assert!(verify_within_range(proof, initial_message, 32));
+        let (initial_message, final_response) =
+            prove_within_range(secret_value as u64, rand_blind, 32)
+                .expect("This shouldn't happen.");
+        assert!(verify_within_range(initial_message, final_response, 32));
 
         // Make sure the second part of the elgamal encryption is the same as the commited value in the range proof.
         let w = CommitmentWitness::try_from((secret_value, rand_blind)).unwrap();
         let elg_secret = ElgamalSecretKey::new(Scalar::random(&mut rng));
         let elg_pub = elg_secret.get_public_key();
         let cipher = elg_pub.encrypt(&w);
-        assert_eq!(initial_message, cipher.y.compress());
+        assert_eq!(initial_message.0, cipher.y.compress());
 
         // Negative test: secret value outside the allowed range
         let large_secret_value: u64 = u64::from(u32::max_value()) + 3;
         let (bad_proof, bad_commitment) =
             prove_within_range(large_secret_value, rand_blind, 32).expect("This shouldn't happen.");
         assert!(!verify_within_range(bad_proof, bad_commitment, 32));
+    }
+
+    #[test]
+    #[wasm_bindgen_test]
+    fn serialize_deserialize_range_proof() {
+        let mut rng = StdRng::from_seed(SEED_1);
+        let secret_value = 42u32;
+        let rand_blind = Scalar::random(&mut rng);
+
+        let (initial_message, final_response) =
+            prove_within_range(secret_value as u64, rand_blind, 32)
+                .expect("This shouldn't happen.");
+
+        let initial_message_bytes: Vec<u8> = serialize(&initial_message).unwrap();
+        let final_response_bytes: Vec<u8> = serialize(&final_response).unwrap();
+        let recovered_initial_message: RangeProofInitialMessage =
+            deserialize(&initial_message_bytes).unwrap();
+        let recovered_final_response: RangeProofFinalResponse =
+            deserialize(&final_response_bytes).unwrap();
+        assert_eq!(recovered_initial_message, initial_message);
+        assert_eq!(
+            final_response_bytes,
+            serialize(&recovered_final_response).unwrap()
+        );
     }
 }
