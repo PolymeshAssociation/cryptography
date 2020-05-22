c97d304b57c58966a5e13d7d15931e8a1ca6163c
ajivanyan <61467068+ajivanyan@users.noreply.github.com>
2020-05-22 10:17:39 +0400

    Initialize the Prover and Verifier objects  with a corresponding `generator` object. (#33)
    * First trial to replace fixed type param with an Enum
    
    * Running cargo fmt
    
    * Minor improvements
    
    * running cargo fmt
    
    * optimizations for improving the code readability
    
    * Initializing the  and  objects with generators
    
    * Addressing the latest review comments
    
    * merging with master

--- cli/cryptography/src/asset_proofs/mod.rs
+++ cli/cryptography/src/asset_proofs/mod.rs
@@ -158,11 +158,11 @@
 //! let pub_key = elg_secret.get_public_key();
 //! let cipher = pub_key.encrypt(&w);
 //!
-//! let prover_0 = CorrectnessProverAwaitingChallenge::new(pub_key, w.clone());
-//! let verifier_0 = CorrectnessVerifier::new(secret_value, pub_key, cipher);
+//! let prover_0 = CorrectnessProverAwaitingChallenge::new(pub_key, w.clone(), &gens);
+//! let verifier_0 = CorrectnessVerifier::new(secret_value, pub_key, cipher, &gens);
 //!
-//! let prover_1 = WellformednessProverAwaitingChallenge { pub_key: pub_key, w: Zeroizing::new(w) };
-//! let verifier_1 = WellformednessVerifier { pub_key, cipher };
+//! let prover_1 = WellformednessProverAwaitingChallenge { pub_key: pub_key, w: Zeroizing::new(w) , pc_gens :&gens};
+//! let verifier_1 = WellformednessVerifier { pub_key, cipher , pc_gens:&gens};
 //!
 //! let mut transcript_rng0 = prover_0.create_transcript_rng(&mut rng, &transcript);
 //! let mut transcript_rng1 =
@@ -170,11 +170,11 @@
 //!
 //! // Provers generate the initial messages
 //! let (prover_0, initial_message0) =
-//!     prover_0.generate_initial_message(&gens, &mut transcript_rng0);
+//!     prover_0.generate_initial_message( &mut transcript_rng0);
 //! initial_message0.update_transcript(&mut transcript).unwrap();
 //!
 //! let (prover_1, initial_message1) =
-//!     prover_1.generate_initial_message(&gens, &mut transcript_rng1);
+//!     prover_1.generate_initial_message( &mut transcript_rng1);
 //! initial_message1.update_transcript(&mut transcript).unwrap();
 //!
 //! // Dealer calculates the challenge from the 2 initial messages
@@ -188,11 +188,11 @@
 //!
 //! // Verifiers verify the proofs
 //! let result =
-//!     verifier_0.verify(&gens, &challenge, &initial_message0, &final_response0);
+//!     verifier_0.verify(&challenge, &initial_message0, &final_response0);
 //! assert!(result.is_ok());
 //!
 //! let result =
-//!     verifier_1.verify(&gens, &challenge, &initial_message1, &final_response1);
+//!     verifier_1.verify(&challenge, &initial_message1, &final_response1);
 //! assert!(result.is_ok());
 //!
 //! ```
