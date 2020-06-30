67c6fb5acbac2f3f7d5867fd9310175fb0b4645b
Parnian <60563484+ParnianAlimi@users.noreply.github.com>
2020-06-30 08:45:51 -0600

    Add Support for Transaction CLI (#55)
    Adds support for:
    * Creating a transaction by the sender account
    * Validating the initialized transaction
    * Finalizing a transaction by the receiver
    * Validating a finalized transaction
    * Justifying the finalized transaction by the mediator
    * Validating the finalized transaction

--- cli/mercat/mediator/src/input.rs
+++ cli/mercat/mediator/src/input.rs
@@ -48,7 +48,7 @@ pub struct CreateMediatorAccountInfo {
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize, StructOpt)]
-pub struct IssueAssetInfo {
+pub struct JustifyIssuanceInfo {
     /// The directory that will serve as the database of the on/off-chain data and will be used
     /// to save and load the data that in a real execution would be written to the on/off the
     /// blockchain. Defaults to the current directory. This directory will have two main
@@ -70,17 +70,16 @@ pub struct IssueAssetInfo {
     /// The name of the issuer.
     /// An account must have already been created for this user, using `mercat-account`
     /// CLI.
-    #[structopt(short, long, help = "The name of the user.")]
+    #[structopt(short, long, help = "The name of the issuer.")]
     pub issuer: String,
 
     /// The name of the mediator.
-    #[structopt(short, long, help = "The name of the user.")]
+    #[structopt(short, long, help = "The name of the mediator.")]
     pub mediator: String,
 
     /// An optional seed, to feed to the RNG, that can be passed to reproduce a previous run of this CLI.
     /// The seed can be found inside the logs.
     #[structopt(
-        short,
         long,
         help = "Base64 encoding of an initial seed for the RNG. If not provided, the seed will be chosen at random."
     )]
@@ -104,12 +103,71 @@ pub struct IssueAssetInfo {
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize, StructOpt)]
+pub struct JustifyTransactionInfo {
+    /// The directory that will serve as the database of the on/off-chain data and will be used
+    /// to save and load the data that in a real execution would be written to the on/off the
+    /// blockchain. Defaults to the current directory. This directory will have two main
+    /// sub-directories: `on-chain` and `off-chain`.
+    #[structopt(
+        parse(from_os_str),
+        help = "The directory to load and save the input and output files. Defaults to current directory.",
+        short,
+        long
+    )]
+    pub db_dir: Option<PathBuf>,
+
+    /// The transaction ID for the asset transaction.
+    /// This ID must be the same as the one used to create the transaction,
+    /// using the `mercat-account` CLI.
+    #[structopt(long, help = "The id of the transaction. This value must be unique.")]
+    pub tx_id: u32,
+
+    /// Asset id that is transferred.
+    /// An asset ticker name which is a string of at most 12 characters.
+    #[structopt(
+        short,
+        long,
+        help = "The asset ticker id. String of at most 12 characters."
+    )]
+    pub ticker_id: String,
+
+    /// The name of the sender.
+    /// An account must have already been created for this user, using `mercat-account`
+    /// CLI.
+    #[structopt(long, help = "The name of the sender.")]
+    pub sender: String,
+
+    /// The name of the mediator.
+    #[structopt(short, long, help = "The name of the mediator.")]
+    pub mediator: String,
+
+    /// Whether to reject a transaction.
+    #[structopt(
+        short,
+        long,
+        help = "If present the mediator will reject the transaction."
+    )]
+    pub reject: bool,
+
+    /// An optional path to save the config used for this experiment.
+    #[structopt(
+        parse(from_os_str),
+        long,
+        help = "Path to save the input command line arguments as a config file."
+    )]
+    pub save_config: Option<PathBuf>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize, StructOpt)]
 pub enum CLI {
     /// Create a MERCAT mediator account.
     Create(CreateMediatorAccountInfo),
 
     /// Justify a MERCAT asset issuance transaction.
-    JustifyIssuance(IssueAssetInfo),
+    JustifyIssuance(JustifyIssuanceInfo),
+
+    /// Justify a MERCAT transaction.
+    JustifyTransaction(JustifyTransactionInfo),
 }
 
 pub fn parse_input() -> Result<CLI, confy::ConfyError> {
@@ -148,7 +206,7 @@ pub fn parse_input() -> Result<CLI, confy::ConfyError> {
 
             let seed: Option<String> = cfg.seed.clone().or_else(|| Some(gen_seed()));
             info!("Seed: {:?}", seed.clone().unwrap());
-            let cfg = IssueAssetInfo {
+            let cfg = JustifyIssuanceInfo {
                 db_dir,
                 tx_id: cfg.tx_id,
                 issuer: cfg.issuer,
@@ -167,6 +225,31 @@ pub fn parse_input() -> Result<CLI, confy::ConfyError> {
             save_config(cfg.save_config.clone(), &cfg);
 
             return Ok(CLI::JustifyIssuance(cfg));
+        }
+
+        CLI::JustifyTransaction(cfg) => {
+            // Set the default seed and db_dir if needed.
+            let db_dir = cfg.db_dir.clone().or_else(|| std::env::current_dir().ok());
+
+            let cfg = JustifyTransactionInfo {
+                db_dir,
+                tx_id: cfg.tx_id,
+                ticker_id: cfg.ticker_id,
+                sender: cfg.sender,
+                mediator: cfg.mediator,
+                reject: cfg.reject,
+                save_config: cfg.save_config.clone(),
+            };
+
+            info!(
+                "Parsed the following config from the command line:\n{:#?}",
+                cfg.clone()
+            );
+
+            // Save the config if the argument is passed.
+            save_config(cfg.save_config.clone(), &cfg);
+
+            return Ok(CLI::JustifyTransaction(cfg));
         }
     }
 }
