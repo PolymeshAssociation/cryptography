26c7e77d44202cd5614b1015ad17ba8b0e250de2
Parnian Alimi <parnian@polymath.network>
2020-07-03 14:02:13 -0600

    fix build issue

--- cryptography/benches/mercat_transaction.rs
+++ cryptography/benches/mercat_transaction.rs
@@ -1,20 +1,16 @@
+mod utility;
 use criterion::{criterion_group, criterion_main, Criterion};
 use cryptography::{
-    asset_proofs::{CommitmentWitness, ElgamalSecretKey},
     mercat::{
-        account::{convert_asset_ids, AccountCreator},
-        asset::{AssetTxIssueMediator, CtxIssuer},
+        account::{convert_asset_ids},
         conf_tx::{CtxMediator, CtxReceiver, CtxSender},
-        Account, AccountCreatorInitializer, AccountMemo, AssetTransactionIssuer,
-        AssetTransactionMediator, AssetTxState, EncryptionKeys, EncryptionPubKey, FinalizedTx,
-        InitializedTx, MediatorAccount, PubAccount, SecAccount, SigningPubKey, TransactionMediator,
+        Account, EncryptionPubKey, FinalizedTx,
+        InitializedTx, MediatorAccount, PubAccount, SigningPubKey, TransactionMediator,
         TransactionReceiver, TransactionSender, TxState, TxSubstate,
     },
     AssetId, Balance,
 };
-use curve25519_dalek::scalar::Scalar;
-use rand::{thread_rng, CryptoRng, RngCore};
-use schnorrkel::{ExpansionMode, MiniSecretKey};
+use rand::thread_rng;
 
 const MIN_SENDER_BALANCE_ORDER: u32 = 1;
 const MAX_SENDER_BALANCE_ORDER: u32 = 3;
@@ -184,8 +180,8 @@ fn bench_transaction(c: &mut Criterion) {
     let valid_asset_ids = convert_asset_ids(valid_asset_ids);
 
     let mut rng = thread_rng();
-    let (public_account, private_account) = generate_mediator_keys(&mut rng);
-    let sender_account = create_secret_account(
+    let (public_account, private_account) = utility::generate_mediator_keys(&mut rng);
+    let sender_account = utility::create_account_with_amount(
         &mut rng,
         &asset_id,
         &valid_asset_ids,
@@ -194,7 +190,7 @@ fn bench_transaction(c: &mut Criterion) {
         0,
     );
     // Create a receiver account and load it with some assets.
-    let receiver_account = create_secret_account(
+    let receiver_account = utility::create_account_with_amount(
         &mut rng,
         &asset_id,
         &valid_asset_ids,
@@ -209,7 +205,7 @@ fn bench_transaction(c: &mut Criterion) {
             let value = 10u32.pow(i);
             Account {
                 scrt: sender_account.scrt.clone(),
-                pblc: issue_assets(
+                pblc: utility::issue_assets(
                     &mut rng,
                     sender_account.clone(),
                     &private_account,
@@ -264,107 +260,3 @@ criterion_group! {
 }
 
 criterion_main!(mercat_transaction);
-
-// todo Make these pub(crate)
-fn issue_assets<R: RngCore + CryptoRng>(
-    rng: &mut R,
-    account: Account,
-    mediator_account: &MediatorAccount,
-    mediator_pub_account: &AccountMemo,
-    amount: u32,
-) -> PubAccount {
-    // Issuer side.
-    let issuer = CtxIssuer {};
-    let (asset_tx, _) = issuer
-        .initialize(
-            0,
-            &account.scrt,
-            &mediator_pub_account.owner_enc_pub_key,
-            amount,
-            rng,
-        )
-        .unwrap();
-
-    // Mediator side.
-    let mediator = AssetTxIssueMediator {};
-    let (_, updated_issuer_account) = mediator
-        .justify(
-            asset_tx.clone(),
-            &account.pblc,
-            AssetTxState::Initialization(TxSubstate::Validated),
-            &mediator_account.encryption_key,
-            &mediator_account.signing_key,
-        )
-        .unwrap();
-    updated_issuer_account
-}
-
-fn generate_mediator_keys<R: RngCore + CryptoRng>(rng: &mut R) -> (AccountMemo, MediatorAccount) {
-    let mediator_elg_secret_key = ElgamalSecretKey::new(Scalar::random(rng));
-    let mediator_enc_key = EncryptionKeys {
-        pblc: mediator_elg_secret_key.get_public_key().into(),
-        scrt: mediator_elg_secret_key.into(),
-    };
-
-    let mediator_signing_pair =
-        MiniSecretKey::generate_with(rng).expand_to_keypair(ExpansionMode::Ed25519);
-
-    (
-        AccountMemo::new(mediator_enc_key.pblc, mediator_signing_pair.public),
-        MediatorAccount {
-            encryption_key: mediator_enc_key,
-            signing_key: mediator_signing_pair,
-        },
-    )
-}
-
-fn create_secret_account<R: RngCore + CryptoRng>(
-    rng: &mut R,
-    asset_id: &AssetId,
-    valid_asset_ids: &Vec<Scalar>,
-    mediator_account: &MediatorAccount,
-    mediator_pub_account: &AccountMemo,
-    initial_amount: u32,
-) -> Account {
-    let secret_account = gen_keys(rng, asset_id);
-
-    let account_creator = AccountCreator {};
-    let account = account_creator
-        .create(secret_account, valid_asset_ids, 0, rng)
-        .unwrap();
-    // If a non-zero initial amount is given issue some assets to this account.
-    if initial_amount > 0 {
-        Account {
-            scrt: account.scrt.clone(),
-            pblc: issue_assets(
-                rng,
-                account,
-                mediator_account,
-                mediator_pub_account,
-                initial_amount,
-            ),
-        }
-    } else {
-        account
-    }
-}
-
-fn gen_keys<R: RngCore + CryptoRng>(rng: &mut R, asset_id: &AssetId) -> SecAccount {
-    let elg_secret = ElgamalSecretKey::new(Scalar::random(rng));
-    let elg_pub = elg_secret.get_public_key();
-    let enc_keys = EncryptionKeys {
-        pblc: elg_pub.into(),
-        scrt: elg_secret.into(),
-    };
-
-    let asset_id_witness = CommitmentWitness::new(asset_id.clone().into(), Scalar::random(rng));
-
-    let sign_keys = MiniSecretKey::generate_with(rng).expand_to_keypair(ExpansionMode::Ed25519);
-
-    SecAccount {
-        enc_keys,
-        sign_keys,
-        asset_id: asset_id.clone(),
-        asset_id_witness,
-    }
-}
---
+++ cryptography/benches/utility.rs
@@ -0,0 +1,133 @@
+use cryptography::{
+    asset_proofs::{CommitmentWitness, ElgamalSecretKey},
+    mercat::{
+        account::{convert_asset_ids, AccountCreator},
+        asset::{AssetTxIssueMediator, CtxIssuer},
+        conf_tx::{CtxMediator, CtxReceiver, CtxSender},
+        Account, AccountCreatorInitializer, AccountMemo, AssetTransactionIssuer,
+        AssetTransactionMediator, AssetTxState, EncryptionKeys, EncryptionPubKey, FinalizedTx,
+        InitializedTx, MediatorAccount, PubAccount, SecAccount, SigningPubKey, TransactionMediator,
+        TransactionReceiver, TransactionSender, TxState, TxSubstate,
+    },
+    AssetId, Balance,
+};
+use curve25519_dalek::scalar::Scalar;
+use rand::{thread_rng, CryptoRng, RngCore};
+use schnorrkel::{ExpansionMode, MiniSecretKey};
+
+pub fn issue_assets<R: RngCore + CryptoRng>(
+    rng: &mut R,
+    account: Account,
+    mediator_account: &MediatorAccount,
+    mediator_pub_account: &AccountMemo,
+    amount: u32,
+) -> PubAccount {
+    // Issuer side.
+    let issuer = CtxIssuer {};
+    let (asset_tx, _) = issuer
+        .initialize(
+            0,
+            &account.scrt,
+            &mediator_pub_account.owner_enc_pub_key,
+            amount,
+            rng,
+        )
+        .unwrap();
+
+    // Mediator side.
+    let mediator = AssetTxIssueMediator {};
+    let (_, updated_issuer_account) = mediator
+        .justify(
+            asset_tx.clone(),
+            &account.pblc,
+            AssetTxState::Initialization(TxSubstate::Validated),
+            &mediator_account.encryption_key,
+            &mediator_account.signing_key,
+        )
+        .unwrap();
+    updated_issuer_account
+}
+
+pub fn generate_mediator_keys<R: RngCore + CryptoRng>(rng: &mut R) -> (AccountMemo, MediatorAccount) {
+    let mediator_elg_secret_key = ElgamalSecretKey::new(Scalar::random(rng));
+    let mediator_enc_key = EncryptionKeys {
+        pblc: mediator_elg_secret_key.get_public_key().into(),
+        scrt: mediator_elg_secret_key.into(),
+    };
+
+    let mediator_signing_pair =
+        MiniSecretKey::generate_with(rng).expand_to_keypair(ExpansionMode::Ed25519);
+
+    (
+        AccountMemo::new(mediator_enc_key.pblc, mediator_signing_pair.public),
+        MediatorAccount {
+            encryption_key: mediator_enc_key,
+            signing_key: mediator_signing_pair,
+        },
+    )
+}
+
+pub fn create_account_with_amount<R: RngCore + CryptoRng>(
+    rng: &mut R,
+    asset_id: &AssetId,
+    valid_asset_ids: &Vec<Scalar>,
+    mediator_account: &MediatorAccount,
+    mediator_pub_account: &AccountMemo,
+    initial_amount: u32,
+) -> Account {
+    let secret_account = gen_keys(rng, asset_id);
+
+    let account_creator = AccountCreator {};
+    let account = account_creator
+        .create(secret_account, valid_asset_ids, 0, rng)
+        .unwrap();
+    // If a non-zero initial amount is given issue some assets to this account.
+    if initial_amount > 0 {
+        Account {
+            scrt: account.scrt.clone(),
+            pblc: issue_assets(
+                rng,
+                account,
+                mediator_account,
+                mediator_pub_account,
+                initial_amount,
+            ),
+        }
+    } else {
+        account
+    }
+}
+
+pub fn create_account<R: RngCore + CryptoRng>(
+    rng: &mut R,
+    asset_id: &AssetId,
+    valid_asset_ids: &Vec<Scalar>,
+    account_id: u32
+) -> Account {
+    let secret_account = gen_keys(rng, asset_id);
+
+    let account_creator = AccountCreator {};
+    account_creator
+        .create(secret_account, valid_asset_ids, account_id, rng)
+        .unwrap()
+}
+
+pub fn gen_keys<R: RngCore + CryptoRng>(rng: &mut R, asset_id: &AssetId) -> SecAccount {
+    let elg_secret = ElgamalSecretKey::new(Scalar::random(rng));
+    let elg_pub = elg_secret.get_public_key();
+    let enc_keys = EncryptionKeys {
+        pblc: elg_pub.into(),
+        scrt: elg_secret.into(),
+    };
+
+    let asset_id_witness = CommitmentWitness::new(asset_id.clone().into(), Scalar::random(rng));
+
+    let sign_keys = MiniSecretKey::generate_with(rng).expand_to_keypair(ExpansionMode::Ed25519);
+
+    SecAccount {
+        enc_keys,
+        sign_keys,
+        asset_id: asset_id.clone(),
+        asset_id_witness,
+    }
+}
