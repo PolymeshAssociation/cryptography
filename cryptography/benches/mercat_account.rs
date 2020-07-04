011ba57442bba8a20ad02a78d8747d98d41f64b7
Parnian Alimi <parnian@polymath.network>
2020-07-03 18:09:39 -0600

    add asset and account benchmarks

---
+++ cryptography/benches/mercat_account.rs
@@ -0,0 +1,54 @@
+mod utility;
+use criterion::{criterion_group, criterion_main, Criterion};
+use cryptography::{
+    mercat::{
+        account::{convert_asset_ids, AccountValidator},
+        AccountCreatorVerifier, PubAccount,
+    },
+    AssetId,
+};
+use rand::thread_rng;
+
+// The size of the valid asset id set.
+const MAX_ASSET_ID_INDEX: u32 = 10000;
+// The asset ids to use for the account.
+// Must be in [0, MAX_ASSET_ID_INDEX)
+const ASSET_IDS: [u32; 4] = [10, 20, 300, 4000];
+
+fn bench_account_creation(c: &mut Criterion) {
+    let valid_asset_ids: Vec<AssetId> = (0..MAX_ASSET_ID_INDEX)
+        .map(|id| AssetId::from(id.clone()))
+        .collect();
+    let valid_asset_ids = convert_asset_ids(valid_asset_ids);
+
+    let mut rng = thread_rng();
+    let public_accounts: Vec<PubAccount> = ASSET_IDS
+        .iter()
+        .map(|&id| utility::create_account(&mut rng, &AssetId::from(id), &valid_asset_ids, 0).pblc)
+        .collect();
+
+    let label = format!("MERCAT Transaction: Validator");
+    c.bench_function_over_inputs(
+        &label,
+        move |b, account| {
+            b.iter(|| {
+                let validator = AccountValidator {};
+                validator.verify(account, &valid_asset_ids).unwrap()
+            })
+        },
+        public_accounts,
+    );
+}
+
+criterion_group! {
+    name = mercat_account;
+    // Lower the sample size to run faster; larger shuffle sizes are
+    // long so we're not microbenchmarking anyways.
+    // 10 is the minimum allowed sample size in Criterion.
+    config = Criterion::default()
+        .sample_size(10);
+        // .measurement_time(Duration::new(60, 0));
+    targets = bench_account_creation,
+}
+
+criterion_main!(mercat_account);
