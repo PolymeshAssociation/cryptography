da0cf31974f025bc3177caba2ee34e9d3f97b2d7
Parnian <60563484+ParnianAlimi@users.noreply.github.com>
2020-09-03 15:59:07 -0600

    Remove RawData. (#81)

--- src/claim_proofs/mod.rs
+++ src/claim_proofs/mod.rs
@@ -5,7 +5,7 @@
 pub mod claim_proofs;
 pub use claim_proofs::{
     build_scope_claim_proof_data, compute_cdd_id, compute_scope_id, CDDClaimData, ProofKeyPair,
-    ProofPublicKey, RawData, ScopeClaimData,
+    ProofPublicKey, ScopeClaimData,
 };
 
 pub mod pedersen_commitments;
@@ -14,26 +14,15 @@ pub use pedersen_commitments::PedersenGenerators;
 use curve25519_dalek::scalar::Scalar;
 use rand_core::{CryptoRng, RngCore};
 
-pub fn random_claim<R: RngCore + CryptoRng + ?Sized>(
-    rng: &mut R,
-) -> (CDDClaimData, ScopeClaimData) {
-    let mut investor_did = RawData::default();
-    let mut investor_unique_id = RawData::default();
-    let mut scope_did = RawData::default();
-
-    rng.fill_bytes(&mut investor_did.0);
-    rng.fill_bytes(&mut investor_unique_id.0);
-    rng.fill_bytes(&mut scope_did.0);
-
-    let investor_unique_id = Scalar::from_bits(investor_unique_id.0);
-
+pub fn random_claim<R: RngCore + CryptoRng>(rng: &mut R) -> (CDDClaimData, ScopeClaimData) {
+    let investor_unique_id = Scalar::random(rng);
     (
         CDDClaimData {
-            investor_did: Scalar::from_bits(investor_did.0),
+            investor_did: Scalar::random(rng),
             investor_unique_id,
         },
         ScopeClaimData {
-            scope_did: Scalar::from_bits(scope_did.0),
+            scope_did: Scalar::random(rng),
             investor_unique_id,
         },
     )
