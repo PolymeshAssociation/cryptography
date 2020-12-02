16da871c4ae9130a830763f0d1010a97e1916f11
miguel <francisco.miguel.garcia.rodriguez@gmail.com>
2020-12-02 12:23:24 +0100

    Use sp_io to facilitate integration

--- confidential-identity/src/mocked.rs
+++ confidential-identity/src/mocked.rs
@@ -1,20 +1,13 @@
 use crate::uuid;
 
-use blake2::{
-    digest::{Update, VariableOutput},
-    VarBlake2b,
-};
+use sp_io::hashing::blake2_128;
 
 /// Create an mocked version of InvestorUid using a DID as input.
 ///
 /// That InvestorUid is just a hash of the given DID, where some bits are updated to be compliant
 /// with UUID v4 spec.
 pub fn make_investor_uid(did: &[u8]) -> [u8; 16] {
-    let mut investor_uid = [0u8; 16];
-
-    let hash = VarBlake2b::new_keyed(&[], 16).chain(did).finalize_boxed();
-
-    investor_uid.copy_from_slice(&hash[..16]);
+    let mut investor_uid = blake2_128(did);
     uuid::set_variant(&mut investor_uid, uuid::Variant::RFC4122);
     uuid::set_version(&mut investor_uid, uuid::Version::V4);
 
