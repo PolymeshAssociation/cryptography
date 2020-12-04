e9214224b10de8afdb8d33ebeffc0ec738f65d1f
Arash Afshar <63250695+aafshar-poly@users.noreply.github.com>
2020-12-04 10:44:39 -0700

    Cryp 187/cleanup dir structure (#97)
    * Splits the cryptography repo into a core library and higher level libraries.
    * Each higher level library exposes various apis/endpoints such as FFI, WASM, and CLI
    
    Co-authored-by: Francisco Miguel García <fmiguelgarcia@users.noreply.github.com>

---
+++ mercat/cli/common/src/account_transfer.rs
@@ -0,0 +1,352 @@
+use crate::{
+    compute_enc_pending_balance, confidential_transaction_file, construct_path,
+    create_rng_from_seed, debug_decrypt, errors::Error, last_ordering_state, load_object,
+    non_empty_account_id, save_object, user_public_account_balance_file, user_public_account_file,
+    user_secret_account_file, OrderedPubAccount, OrderedTransferInstruction, OrderingState,
+    PrintableAccountId, COMMON_OBJECTS_DIR, MEDIATOR_PUBLIC_ACCOUNT_FILE, OFF_CHAIN_DIR,
+    ON_CHAIN_DIR,
+};
+use base64;
+use codec::{Decode, Encode};
+use log::{debug, info};
+use mercat::{
+    transaction::{CtxReceiver, CtxSender},
+    Account, EncryptedAmount, EncryptionPubKey, InitializedTransferTx, PubAccount,
+    TransferTransactionReceiver, TransferTransactionSender, TransferTxState, TxSubstate,
+};
+use metrics::timing;
+use rand::Rng;
+use std::{path::PathBuf, time::Instant};
+
+pub fn process_create_tx(
+    seed: String,
+    db_dir: PathBuf,
+    sender: String,
+    receiver: String,
+    mediator: String,
+    ticker: String,
+    amount: u32,
+    stdout: bool,
+    tx_id: u32,
+    cheat: bool,
+) -> Result<(), Error> {
+    let mut rng = create_rng_from_seed(Some(seed))?;
+    let load_from_file_timer = Instant::now();
+
+    let sender_ordered_pub_account: OrderedPubAccount = load_object(
+        db_dir.clone(),
+        ON_CHAIN_DIR,
+        &sender,
+        &user_public_account_file(&ticker),
+    )?;
+    let sender_account_balance: EncryptedAmount = load_object(
+        db_dir.clone(),
+        ON_CHAIN_DIR,
+        &sender,
+        &user_public_account_balance_file(&ticker),
+    )?;
+    let sender_account = Account {
+        secret: load_object(
+            db_dir.clone(),
+            OFF_CHAIN_DIR,
+            &sender,
+            &user_secret_account_file(&ticker),
+        )?,
+        public: sender_ordered_pub_account.pub_account,
+    };
+
+    let receiver_account: OrderedPubAccount = load_object(
+        db_dir.clone(),
+        ON_CHAIN_DIR,
+        &receiver,
+        &user_public_account_file(&ticker),
+    )?;
+
+    let mediator_account: EncryptionPubKey = load_object(
+        db_dir.clone(),
+        ON_CHAIN_DIR,
+        &mediator,
+        MEDIATOR_PUBLIC_ACCOUNT_FILE,
+    )?;
+
+    timing!(
+        "account.create_tx.load_from_file",
+        load_from_file_timer,
+        Instant::now(),
+        "tx_id" => tx_id.to_string()
+    );
+
+    // Calculate the pending
+    let calc_pending_state_timer = Instant::now();
+    let last_processed_tx_counter = sender_ordered_pub_account.last_processed_tx_counter;
+    let last_processed_account_balance = sender_account_balance;
+    let ordering_state = last_ordering_state(
+        sender.clone(),
+        last_processed_tx_counter,
+        tx_id,
+        db_dir.clone(),
+    )?;
+
+    let pending_balance = compute_enc_pending_balance(
+        &sender,
+        ordering_state.clone(),
+        last_processed_tx_counter,
+        last_processed_account_balance,
+        db_dir.clone(),
+    )?;
+    debug!(
+        "------------> initiating transfer tx: {}, pending_balance: {}",
+        tx_id,
+        debug_decrypt(
+            sender_account.public.enc_asset_id,
+            pending_balance.clone(),
+            db_dir.clone()
+        )?
+    );
+    let next_pending_tx_counter = ordering_state.last_pending_tx_counter + 1;
+
+    timing!(
+        "account.create_tx.calc_pending_state",
+        calc_pending_state_timer,
+        Instant::now(),
+        "tx_id" => tx_id.to_string()
+    );
+
+    let mut amount = amount;
+    // To simplify the cheating selection process, we randomly choose a cheating strategy,
+    // instead of requiring the caller to know of all the different cheating strategies.
+    let cheating_strategy: u32 = rng.gen_range(0, 2);
+
+    // The first cheating strategies make changes to the input, while the subsequent ones
+    // changes the output.
+    if cheat && cheating_strategy == 0 {
+        info!(
+            "CLI log: tx-{}: Cheating by changing the agreed upon amount. Correct amount: {}",
+            tx_id, amount
+        );
+        amount += 1
+    }
+
+    // Initialize the transaction.
+    let create_tx_timer = Instant::now();
+    let ctx_sender = CtxSender {};
+    let pending_account = Account {
+        secret: sender_account.secret,
+        public: PubAccount {
+            enc_asset_id: sender_account.public.enc_asset_id,
+            owner_enc_pub_key: sender_account.public.owner_enc_pub_key,
+        },
+    };
+    let mut asset_tx = ctx_sender
+        .create_transaction(
+            &pending_account,
+            &pending_balance,
+            &receiver_account.pub_account,
+            &mediator_account,
+            &[],
+            amount,
+            &mut rng,
+        )
+        .map_err(|error| Error::LibraryError { error })?;
+
+    let ordering_state = OrderingState {
+        last_processed_tx_counter: sender_ordered_pub_account.last_processed_tx_counter,
+        last_pending_tx_counter: next_pending_tx_counter,
+        tx_id,
+    };
+    timing!("account.create_tx.create", create_tx_timer, Instant::now());
+
+    if cheat && cheating_strategy == 1 {
+        info!(
+            "CLI log: tx-{}: Cheating by changing the sender's account id. Correct account id: {}",
+            tx_id,
+            PrintableAccountId(pending_account.public.enc_asset_id.encode())
+        );
+        asset_tx.memo.sender_account_id += non_empty_account_id();
+    }
+
+    // Save the artifacts to file.
+    let new_state = TransferTxState::Initialization(TxSubstate::Started);
+    let save_to_file_timer = Instant::now();
+    let instruction = OrderedTransferInstruction {
+        state: new_state,
+        ordering_state,
+        data: asset_tx.encode().to_vec(),
+    };
+
+    save_object(
+        db_dir,
+        ON_CHAIN_DIR,
+        COMMON_OBJECTS_DIR,
+        &confidential_transaction_file(tx_id, &sender, new_state),
+        &instruction,
+    )?;
+
+    if stdout {
+        info!(
+            "CLI log: tx-{}: Transaction as base64:\n{}\n",
+            tx_id,
+            base64::encode(asset_tx.encode())
+        );
+    }
+
+    timing!(
+        "account.create_tx.save_to_file",
+        save_to_file_timer,
+        Instant::now()
+    );
+
+    Ok(())
+}
+
+pub fn process_finalize_tx(
+    seed: String,
+    db_dir: PathBuf,
+    sender: String,
+    receiver: String,
+    ticker: String,
+    amount: u32,
+    stdout: bool,
+    tx_id: u32,
+    cheat: bool,
+) -> Result<(), Error> {
+    let mut rng = create_rng_from_seed(Some(seed))?;
+    let load_from_file_timer = Instant::now();
+    let state = TransferTxState::Initialization(TxSubstate::Started);
+
+    let receiver_ordered_pub_account: OrderedPubAccount = load_object(
+        db_dir.clone(),
+        ON_CHAIN_DIR,
+        &receiver,
+        &user_public_account_file(&ticker),
+    )?;
+    let receiver_account = Account {
+        secret: load_object(
+            db_dir.clone(),
+            OFF_CHAIN_DIR,
+            &receiver,
+            &user_secret_account_file(&ticker),
+        )?,
+        public: receiver_ordered_pub_account.pub_account,
+    };
+
+    let instruction: OrderedTransferInstruction = load_object(
+        db_dir.clone(),
+        ON_CHAIN_DIR,
+        COMMON_OBJECTS_DIR,
+        &confidential_transaction_file(tx_id.clone(), &sender, state),
+    )?;
+
+    let tx = InitializedTransferTx::decode(&mut &instruction.data[..]).map_err(|error| {
+        Error::ObjectLoadError {
+            error,
+            path: construct_path(
+                db_dir.clone(),
+                ON_CHAIN_DIR,
+                &sender.clone(),
+                &confidential_transaction_file(tx_id.clone(), &sender, state),
+            ),
+        }
+    })?;
+
+    timing!(
+        "account.finalize_tx.load_from_file",
+        load_from_file_timer,
+        Instant::now(),
+        "tx_id" => tx_id.to_string()
+    );
+
+    // Calculate the pending
+    let calc_pending_state_timer = Instant::now();
+    let ordering_state = last_ordering_state(
+        receiver,
+        receiver_ordered_pub_account.last_processed_tx_counter,
+        tx_id,
+        db_dir.clone(),
+    )?;
+    let next_pending_tx_counter = ordering_state.last_pending_tx_counter + 1;
+
+    timing!(
+        "account.finalize_tx.calc_pending_state",
+        calc_pending_state_timer,
+        Instant::now(),
+        "tx_id" => tx_id.to_string()
+    );
+
+    let mut amount = amount;
+    // To simplify the cheating selection process, we randomly choose a cheating strategy,
+    // instead of requiring the caller to know of all the different cheating strategies.
+    let cheating_strategy: u32 = rng.gen_range(0, 2);
+
+    // The first cheating strategies make changes to the input, while the 2nd one
+    // changes the output.
+    if cheat && cheating_strategy == 0 {
+        info!(
+            "CLI log: tx-{}: Cheating by changing the agreed upon amount. Correct amount: {}",
+            tx_id, amount
+        );
+        amount += 1
+    }
+
+    // Finalize the transaction.
+    let finalize_by_receiver_timer = Instant::now();
+    let receiver = CtxReceiver {};
+    let mut asset_tx = receiver
+        .finalize_transaction(tx, receiver_account.clone(), amount, &mut rng)
+        .map_err(|error| Error::LibraryError { error })?;
+
+    let ordering_state = OrderingState {
+        last_processed_tx_counter: receiver_ordered_pub_account.last_processed_tx_counter,
+        last_pending_tx_counter: next_pending_tx_counter,
+        tx_id,
+    };
+
+    if cheat && cheating_strategy == 1 {
+        info!(
+            "CLI log: tx-{}: Cheating by changing the receiver's account id. Correct account id: {}",
+            tx_id, PrintableAccountId(receiver_account.public.enc_asset_id.encode())
+        );
+        asset_tx.init_data.memo.receiver_account_id += non_empty_account_id();
+    }
+
+    timing!(
+        "account.finalize_tx.finalize_by_receiver",
+        finalize_by_receiver_timer,
+        Instant::now(),
+        "tx_id" => tx_id.to_string()
+    );
+
+    // Save the artifacts to file.
+    let save_to_file_timer = Instant::now();
+    let state = TransferTxState::Finalization(TxSubstate::Started);
+    let instruction = OrderedTransferInstruction {
+        state,
+        ordering_state,
+        data: asset_tx.encode().to_vec(),
+    };
+
+    save_object(
+        db_dir,
+        ON_CHAIN_DIR,
+        COMMON_OBJECTS_DIR,
+        &confidential_transaction_file(tx_id, &sender, state),
+        &instruction,
+    )?;
+
+    if stdout {
+        info!(
+            "CLI log: tx-{}: Transaction as base64:\n{}\n",
+            tx_id,
+            base64::encode(asset_tx.encode())
+        );
+    }
+
+    timing!(
+        "account.finalize_tx.save_to_file",
+        save_to_file_timer,
+        Instant::now(),
+        "tx_id" => tx_id.to_string()
+    );
+
+    Ok(())
+}
---
+++ mercat/cli/common/src/errors.rs
@@ -0,0 +1,159 @@
+use crate::CoreTransaction;
+use failure::Fail;
+use std::path::PathBuf;
+
+/// Common errors for all mercat clis
+#[derive(Fail, Debug)]
+pub enum Error {
+    /// Instruction is not valid.
+    #[fail(display = "Instruction is not valid.")]
+    InvalidInstructionError,
+
+    /// The create account has been called with an empty seed.
+    #[fail(display = "The input seed cannot be empty.")]
+    EmptySeed,
+
+    /// There was an error in converting the seed from base64 to byte array.
+    #[fail(display = "Error in decoding the seed value: {:?}", error)]
+    SeedDecodeError { error: base64::DecodeError },
+
+    /// The length of the provided seed was not equal to 32 bytes.
+    #[fail(display = "Want seed length 32, got len: {:?}", length)]
+    SeedLengthError { length: usize },
+
+    /// An error occurred while deserializing asset id list to a vector of Scalar values.
+    #[fail(display = "Could not deserialize the asset id list from {:?}", path)]
+    AssetIdListDeserializeError { path: String },
+
+    /// An error occurred during the call to the mercat library.
+    #[fail(display = "An error occurred in the underlying library: {:?}", error)]
+    LibraryError {
+        error: cryptography_core::errors::Error,
+    },
+
+    /// No database directory path was passed.
+    #[fail(display = "The database directory must be provided.")]
+    EmptyDatabaseDir,
+
+    /// An error occurred while creating an empty file.
+    #[fail(display = "Failed to create the file {:?}: {:?}", path, error)]
+    FileCreationError {
+        error: std::io::Error,
+        path: PathBuf,
+    },
+
+    /// An error occurred while reading from a file.
+    #[fail(display = "Failed to read the file {:?}: {:?}", path, error)]
+    FileReadError {
+        error: std::io::Error,
+        path: PathBuf,
+    },
+
+    /// An error occurred while deserializing an object from a file.
+    #[fail(
+        display = "Failed to deserialize an object, read from file {:?}: {:?}",
+        path, error
+    )]
+    ObjectDeserializationError {
+        error: serde_json::Error,
+        path: PathBuf,
+    },
+
+    /// An error occurred while writing to a file.
+    #[fail(display = "Failed to write to file {:?}: {:?}", path, error)]
+    FileWriteError {
+        error: serde_json::Error,
+        path: PathBuf,
+    },
+
+    /// An error occurred while decoding an object.
+    #[fail(
+        display = "Failed to decode an object, read from file {:?}: {:?}",
+        path, error
+    )]
+    ObjectLoadError { error: codec::Error, path: PathBuf },
+
+    /// An error occurred while writing to a file.
+    #[fail(display = "Failed to save data to file {:?}: {:?}", path, error)]
+    ObjectSaveError {
+        error: std::io::Error,
+        path: PathBuf,
+    },
+
+    /// An error occurred while removing a file.
+    #[fail(display = "Failed to remove file {:?}: {:?}", path, error)]
+    FileRemovalError {
+        error: std::io::Error,
+        path: PathBuf,
+    },
+
+    /// An error occurred while reading from a file.
+    #[fail(
+        display = "Failed to parse the config file {:?}, because {:?}",
+        path, reason
+    )]
+    ErrorParsingTestHarnessConfig { path: PathBuf, reason: String },
+
+    /// An error occurred while parsing regex.
+    #[fail(display = "Failed to parse the regex: {:?}", reason)]
+    RegexError { reason: String },
+
+    /// Balance great than u32
+    #[fail(display = "balance does not fit u32")]
+    BalanceTooBig,
+
+    /// There can be only one top level transaction
+    #[fail(display = "There can be only one top level transaction")]
+    TopLevelTransaction,
+
+    /// Could not convert a PathBuf to string
+    #[fail(display = "Could not convert a PathBuf to string")]
+    PathBufConversionError,
+
+    /// Error accessing the glob pattern.
+    #[fail(display = "Error accessing the glob pattern")]
+    GlobPatternError,
+
+    /// Error in decoding the object.
+    #[fail(display = "Error in decoding the object.")]
+    DecodeError,
+
+    /// Could not find account id in the validators local map.
+    #[fail(
+        display = "Could not find account id {} in the validator's local map.",
+        account_id
+    )]
+    AccountIdNotFound { account_id: String },
+
+    /// Invalid transaction file
+    #[fail(display = "Invalid transaction file: {}.", path)]
+    InvalidTransactionFile { path: String },
+
+    /// Transaction is not ready for validation
+    #[fail(display = "Transaction is not ready for validation: {:?}.", tx)]
+    TransactionIsNotReadyForValidation { tx: CoreTransaction },
+
+    /// Last transaction could not be found for user.
+    #[fail(display = "Last transaction could not be found for user: {:?}.", user)]
+    LastTransactionNotFound { user: String },
+
+    /// Last processed tx error.
+    #[fail(
+        display = "Last processed tx counter in the transaction cannot be less than the last processed tx counter in the account. Want {:?} > {:?}",
+        current, earliest
+    )]
+    MismatchInProcessedCounter {
+        current: Option<u32>,
+        earliest: Option<u32>,
+    },
+
+    /// Last processed tx counter should not be less than -1
+    #[fail(
+        display = "Last processed tx counter should not be less than -1, got {}.",
+        value
+    )]
+    InvalidLastProcessedTxCounter { value: i32 },
+
+    #[fail(display = "Not implemented, story: {}", story)]
+    NotImplemented { story: String },
+}
