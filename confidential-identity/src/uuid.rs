9d9606816c8b12aba0e9c2e1cee2908e1a631626
Parnian Alimi <parnian@polymath.network>
2020-12-09 14:22:38 -0700

    remove boilerplate

--- confidential-identity/src/uuid.rs
+++ confidential-identity/src/uuid.rs
@@ -1,15 +1,3 @@
-// This file is part of the Polymesh distribution (https://github.com/PolymathNetwork/Polymesh).
-// Copyright (c) 2020 Polymath
-
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, version 3.
-
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-// General Public License for more details.
-
 /// It contains some constants to work with UUID.
 
 /// Fields' indexes of the UUID, encoded as 16 octects.
