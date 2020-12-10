21709fc8588a8bd0697e2ce0231dc301a02651c7
Arash Afshar <arash@polymath.network>
2020-12-10 11:42:08 -0700

    first draft

---
+++ mercat/wasm/README.md
@@ -0,0 +1,6 @@
+# WASM bindings for MERCAT Library
+
+
+This library provides WASM binding for MERCAT.
+
+To build this library, run `wasm-pack build --release`
---
+++ mercat/wasm/src/lib.rs
@@ -0,0 +1,324 @@
+use base64;
+use codec::{Decode, Encode};
+use mercat::{
+    account::{convert_asset_ids, AccountCreator},
+    asset::AssetIssuer,
+    cryptography_core::{
+        asset_proofs::{CipherText, CommitmentWitness, ElgamalPublicKey, ElgamalSecretKey},
+        curve25519_dalek::scalar::Scalar,
+        errors::Error,
+        AssetId,
+    },
+    transaction::{CtxMediator, CtxReceiver, CtxSender},
+    Account as MercatAccount, AccountCreatorInitializer, AssetTransactionIssuer, EncryptionKeys,
+    FinalizedTransferTx, InitializedTransferTx, MediatorAccount, PubAccount as MercatPubAccount,
+    SecAccount, TransferTransactionMediator, TransferTransactionReceiver,
+    TransferTransactionSender,
+};
+use rand::{rngs::StdRng, SeedableRng};
+use rand_core::{OsRng, RngCore};
+use serde::{Deserialize, Serialize};
+use wasm_bindgen::prelude::*;
+
+pub type PlainHex = String;
+
+pub type Base64 = String;
+
+#[wasm_bindgen]
+pub struct CreatAccountOutput {
+    secret_account: Base64,
+    account_id: Base64,
+    account_tx: Base64,
+}
+
+#[wasm_bindgen]
+pub struct CreatMediatorAccountOutput {
+    secret_account: Base64,
+    public_account: Base64,
+}
+
+#[wasm_bindgen]
+pub struct CreateTransactionOutput {
+    init_tx: Base64,
+}
+
+#[wasm_bindgen]
+pub struct FinalizedTransactionOutput {
+    finalized_tx: Base64,
+}
+
+#[wasm_bindgen]
+pub struct MintAssetOutput {
+    asset_tx: Base64,
+}
+
+#[wasm_bindgen]
+pub struct ValidAssetIds {
+    plain_hex_ids: Vec<PlainHex>,
+}
+
+#[wasm_bindgen]
+pub struct Account {
+    secret_account: Base64,
+    public_account: PubAccount,
+}
+
+#[wasm_bindgen]
+pub struct PubAccount {
+    account_id: Base64,
+    public_key: Base64,
+}
+
+impl PubAccount {
+    fn to_mercat(&self) -> MercatPubAccount {
+        let decoded = base64::decode(&self.public_key).unwrap();
+        let owner_enc_pub_key = ElgamalPublicKey::decode(&mut &decoded[..]).unwrap();
+
+        let decoded = base64::decode(&self.account_id).unwrap();
+        let enc_asset_id = CipherText::decode(&mut &decoded[..]).unwrap();
+
+        MercatPubAccount {
+            owner_enc_pub_key,
+            enc_asset_id,
+        }
+    }
+}
+
+impl Account {
+    fn to_mercat(&self) -> MercatAccount {
+        let decoded = base64::decode(&self.secret_account).unwrap();
+        let secret = SecAccount::decode(&mut &decoded[..]).unwrap();
+
+        MercatAccount {
+            secret,
+            public: self.public_account.to_mercat(),
+        }
+    }
+}
+
+/// TODO
+///
+/// # Arguments
+/// * `todo`: todo
+///
+/// # Outputs
+/// * `todo`: todo
+///
+/// # Errors
+/// * todo
+#[wasm_bindgen]
+pub fn create_account(
+    valid_ticker_ids: ValidAssetIds,
+    ticker_id: PlainHex,
+) -> Result<CreatAccountOutput, JsValue> {
+    let mut rng = OsRng;
+
+    let secret_account = create_secret_account(&mut rng, ticker_id.clone()).unwrap(); // TODO
+    let valid_asset_ids: Vec<AssetId> = valid_ticker_ids
+        .plain_hex_ids
+        .into_iter()
+        .map(|ticker_id| {
+            let mut asset_id = [0u8; 12];
+            let decoded = hex::decode(ticker_id).unwrap(); // TODO
+            asset_id[..decoded.len()].copy_from_slice(&decoded);
+            Ok(AssetId { id: asset_id })
+        })
+        .collect::<Result<Vec<AssetId>, Error>>()
+        .unwrap();
+    let valid_asset_ids = convert_asset_ids(valid_asset_ids);
+    let account_tx = AccountCreator
+        .create(&secret_account, &valid_asset_ids, &mut rng)
+        .unwrap();
+    let account_id = account_tx.pub_account.enc_asset_id.clone();
+
+    Ok(CreatAccountOutput {
+        secret_account: base64::encode(secret_account.encode()),
+        account_id: base64::encode(account_id.encode()),
+        account_tx: base64::encode(account_tx.encode()),
+    })
+}
+
+/// TODO
+///
+/// # Arguments
+/// * `todo`: todo
+///
+/// # Outputs
+/// * `todo`: todo
+///
+/// # Errors
+/// * todo
+#[wasm_bindgen]
+pub fn create_mediator_account() -> CreatMediatorAccountOutput {
+    let mut rng = OsRng;
+
+    let mediator_elg_secret_key = ElgamalSecretKey::new(Scalar::random(&mut rng));
+    let mediator_enc_key = EncryptionKeys {
+        public: mediator_elg_secret_key.get_public_key().into(),
+        secret: mediator_elg_secret_key.into(),
+    };
+
+    CreatMediatorAccountOutput {
+        public_account: base64::encode(mediator_enc_key.public.encode()),
+        secret_account: base64::encode(
+            MediatorAccount {
+                encryption_key: mediator_enc_key,
+            }
+            .encode(),
+        ),
+    }
+}
+
+/// TODO
+///
+/// # Arguments
+/// * `todo`: todo
+///
+/// # Outputs
+/// * `todo`: todo
+///
+/// # Errors
+/// * todo
+#[wasm_bindgen]
+pub fn mint_asset(amount: u32, issuer_account: Account) -> MintAssetOutput {
+    let mut rng = OsRng;
+    let asset_tx = AssetIssuer
+        .initialize_asset_transaction(&issuer_account.to_mercat(), &[], amount, &mut rng)
+        .unwrap(); // TODO
+
+    MintAssetOutput {
+        asset_tx: base64::encode(asset_tx.encode()),
+    }
+}
+
+/// TODO
+///
+/// # Arguments
+/// * `todo`: todo
+///
+/// # Outputs
+/// * `todo`: todo
+///
+/// # Errors
+/// * todo
+#[wasm_bindgen]
+pub fn create_transaction(
+    amount: u32,
+    sender_account: Account,
+    encrypted_pending_balance: Base64,
+    receiver_public_account: PubAccount,
+    mediator_public_key: Base64,
+) -> CreateTransactionOutput {
+    let mut rng = OsRng;
+
+    let decoded = base64::decode(encrypted_pending_balance).unwrap();
+    let pending_balance = CipherText::decode(&mut &decoded[..]).unwrap();
+
+    let decoded = base64::decode(mediator_public_key).unwrap();
+    let mediator_public_key = ElgamalPublicKey::decode(&mut &decoded[..]).unwrap();
+
+    let init_tx = CtxSender
+        .create_transaction(
+            &sender_account.to_mercat(),
+            &pending_balance,
+            &receiver_public_account.to_mercat(),
+            &mediator_public_key,
+            &[],
+            amount,
+            &mut rng,
+        )
+        .unwrap();
+
+    CreateTransactionOutput {
+        init_tx: base64::encode(init_tx.encode()),
+    }
+}
+
+/// TODO
+///
+/// # Arguments
+/// * `todo`: todo
+///
+/// # Outputs
+/// * `todo`: todo
+///
+/// # Errors
+/// * todo
+#[wasm_bindgen]
+pub fn finalize_transaction(
+    amount: u32,
+    init_tx: Base64,
+    receiver_account: Account,
+) -> FinalizedTransactionOutput {
+    let mut rng = OsRng;
+
+    let decoded = base64::decode(init_tx).unwrap();
+    let tx = InitializedTransferTx::decode(&mut &decoded[..]).unwrap();
+
+    let finalized_tx = CtxReceiver
+        .finalize_transaction(tx, receiver_account.to_mercat(), amount, &mut rng)
+        .unwrap();
+
+    FinalizedTransactionOutput {
+        finalized_tx: base64::encode(finalized_tx.encode()),
+    }
+}
+
+///// TODO
+/////
+///// # Arguments
+///// * `todo`: todo
+/////
+///// # Outputs
+///// * `todo`: todo
+/////
+///// # Errors
+///// * todo
+//#[wasm_bindgen]
+//pub fn justify_transaction(
+//    finalized_tx: Base64,
+//    mediator_account: MediatorAccount,
+//) -> FinalizedTransactionOutput {
+//    let mut rng = OsRng;
+//
+//    let decoded = base64::decode(finalized_tx).unwrap();
+//    let finalized_tx = FinalizedTransferTx::decode(&mut &decoded[..]).unwrap();
+//
+//    let justified_tx = CtxMediator
+//        .justify_transaction(
+//            finalized_tx,
+//            &mediator_account.encryption_key,
+//            &sender_pub_account,
+//            &sender_balance,
+//            &receiver_pub_account,
+//            &[],
+//            asset_id,
+//            &mut rng,
+//        )
+//        .unwrap();
+//
+//    FinalizedTransactionOutput {
+//        finalized_tx: base64::encode(finalized_tx.encode()),
+//    }
+//}
+
+fn create_secret_account(rng: &mut OsRng, ticker_id: String) -> Result<SecAccount, Error> {
+    let elg_secret = ElgamalSecretKey::new(Scalar::random(rng));
+    let elg_pub = elg_secret.get_public_key();
+    let enc_keys = EncryptionKeys {
+        public: elg_pub.into(),
+        secret: elg_secret.into(),
+    };
+
+    let mut asset_id = [0u8; 12];
+    let decoded = hex::decode(ticker_id).unwrap();
+    asset_id[..decoded.len()].copy_from_slice(&decoded);
+
+    let asset_id = AssetId { id: asset_id };
+    let asset_id_witness = CommitmentWitness::new(asset_id.clone().into(), Scalar::random(rng));
+
+    Ok(SecAccount {
+        enc_keys,
+        asset_id_witness,
+    })
+}
