6c234abde9072770205ec3f935da6b9775b0d0a0
Arash Afshar <arash@polymath.network>
2021-01-18 18:46:28 -0700

    compress the artifacts

--- scripts/artifacts.sh
+++ scripts/artifacts.sh
@@ -2,6 +2,7 @@
 
 ROOT=$( cd `dirname $0`/..;  pwd )
 ARTIFACT_DIR="artifacts"
+mkdir $ARTIFACT_DIR
 
 echo "Building the binaries..."
 cd "$ROOT"
@@ -17,8 +18,11 @@ cd     "$CORE_ROOT"
 
 # ====== Rust Library Source code
 echo "Packing core source files..."
-mkdir -p "$ARTIFACT_DIR/sources/cryptography-core"
-find . -type f -not -path "./target/*" -not -path "./artifacts/*" -exec cp --parents {} "$ARTIFACT_DIR/sources/cryptography-core" \;
+mkdir -p "$ARTIFACT_DIR/src/cryptography-core"
+find . -type f -not -path "./target/*" -not -path "./artifacts/*" -exec cp --parents {} "$ARTIFACT_DIR/src/cryptography-core" \;
+tar -czvf cryptography-core-src.tar.gz "$ARTIFACT_DIR/src"
+mv cryptography-core-src.tar.gz "$ROOT/$ARTIFACT_DIR"
+rm -rf "$ARTIFACT_DIR/src"
 
 # ====== Rust Library Crate
 echo "Packing core lib... SKIPPED"
@@ -35,10 +39,13 @@ cd     "$MERCAT_ROOT"
 
 # ====== Rust Library Source code
 echo "Packing MERCAT source files..."
-mkdir -p "$ARTIFACT_DIR/sources/cryptography-core"
-mkdir -p "$ARTIFACT_DIR/sources/mercat"
-find ../cryptography-core -type f -not -path "../cryptography-core/target/*" -not -path "../cryptography-core/artifacts/*" -exec cp --parents {} "$ARTIFACT_DIR/sources/cryptography-core" \;
-find . -type f -not -path "./target/*" -not -path "./artifacts/*" -not -path "./wasm/*" -not -path "./cli/*" -exec cp --parents {} "$ARTIFACT_DIR/sources/mercat" \;
+mkdir -p "$ARTIFACT_DIR/src/cryptography-core"
+mkdir -p "$ARTIFACT_DIR/src/mercat"
+find ../cryptography-core -type f -not -path "../cryptography-core/target/*" -not -path "../cryptography-core/artifacts/*" -exec cp --parents {} "$ARTIFACT_DIR/src/cryptography-core" \;
+find . -type f -not -path "./target/*" -not -path "./artifacts/*" -not -path "./wasm/*" -not -path "./cli/*" -exec cp --parents {} "$ARTIFACT_DIR/src/mercat" \;
+tar -czvf mercat-src.tar.gz "$ARTIFACT_DIR/src"
+mv mercat-src.tar.gz "$ROOT/$ARTIFACT_DIR"
+rm -rf "$ARTIFACT_DIR/src"
 
 # ====== Rust Library Crate
 echo "Packing MERCAT lib... SKIPPED"
@@ -51,6 +58,9 @@ cd $ROOT/mercat/wasm
 wasm-pack build --release
 cp -r pkg/* "$ROOT/mercat/$ARTIFACT_DIR/npm/"
 cd -
+tar -czvf mercat-npm.tar.gz "$ARTIFACT_DIR/npm"
+mv mercat-npm.tar.gz "$ROOT/$ARTIFACT_DIR"
+rm -rf "$ARTIFACT_DIR/npm"
 # TODO: publish to npm if run by CI
 
 # ====== CLI Executables
@@ -62,6 +72,9 @@ cp "$ROOT/target/release/mercat-chain-setup" "$ARTIFACT_DIR/bin/linux-x64/"
 cp "$ROOT/target/release/mercat-mediator"    "$ARTIFACT_DIR/bin/linux-x64/"
 cp "$ROOT/target/release/mercat-account"     "$ARTIFACT_DIR/bin/linux-x64/"
 cp "$ROOT/target/release/mercat-validator"   "$ARTIFACT_DIR/bin/linux-x64/"
+tar -czvf mercat-bin.tar.gz "$ARTIFACT_DIR/bin"
+mv mercat-bin.tar.gz "$ROOT/$ARTIFACT_DIR"
+rm -rf "$ARTIFACT_DIR/bin"
 
 
 # ---------------------------------------------------------------------------------
@@ -74,10 +87,13 @@ cd     "$CONFIDENTIAL_IDENTITY_ROOT"
 
 # ====== Rust Library Source code
 echo "Packing Confidential Identity source files..."
-mkdir -p "$ARTIFACT_DIR/sources/cryptography-core"
-mkdir -p "$ARTIFACT_DIR/sources/confidential-identity"
-find ../cryptography-core -type f -not -path "../cryptography-core/target/*" -not -path "../cryptography-core/artifacts/*" -exec cp --parents {} "$ARTIFACT_DIR/sources/cryptography-core" \;
-find . -type f -not -path "./target/*" -not -path "./artifacts/*" -not -path "./wasm/*" -not -path "./cli/*" -not -path "./ffi/*" -exec cp --parents {} "$ARTIFACT_DIR/sources/confidential-identity" \;
+mkdir -p "$ARTIFACT_DIR/src/cryptography-core"
+mkdir -p "$ARTIFACT_DIR/src/confidential-identity"
+find ../cryptography-core -type f -not -path "../cryptography-core/target/*" -not -path "../cryptography-core/artifacts/*" -exec cp --parents {} "$ARTIFACT_DIR/src/cryptography-core" \;
+find . -type f -not -path "./target/*" -not -path "./artifacts/*" -not -path "./wasm/*" -not -path "./cli/*" -not -path "./ffi/*" -exec cp --parents {} "$ARTIFACT_DIR/src/confidential-identity" \;
+tar -czvf confidential-identity-src.tar.gz "$ARTIFACT_DIR/src"
+mv confidential-identity-src.tar.gz "$ROOT/$ARTIFACT_DIR"
+rm -rf "$ARTIFACT_DIR/src"
 
 # ====== Rust Library Crate
 echo "Packing Confidential Identity lib... SKIPPED"
@@ -90,6 +106,9 @@ cd $ROOT/confidential-identity/wasm
 wasm-pack build --release
 cp -r pkg/* "$ROOT/confidential-identity/$ARTIFACT_DIR/npm/"
 cd -
+tar -czvf confidential-identity-npm.tar.gz "$ARTIFACT_DIR/npm"
+mv confidential-identity-npm.tar.gz "$ROOT/$ARTIFACT_DIR"
+rm -rf "$ARTIFACT_DIR/npm"
 # TODO: publish to npm if run by CI
 
 # ====== C Libraries
@@ -97,6 +116,9 @@ echo "Packing Confidential Identity C bindings..."
 mkdir -p "$ARTIFACT_DIR/ffi"
 cp "$ROOT/target/release/libconfidential_identity_ffi.so" "$ARTIFACT_DIR/ffi"
 cp "$CONFIDENTIAL_IDENTITY_ROOT/ffi/confidential_identity.h" "$ARTIFACT_DIR/ffi"
+tar -czvf confidential-identity-ffi.tar.gz "$ARTIFACT_DIR/ffi"
+mv confidential-identity-ffi.tar.gz "$ROOT/$ARTIFACT_DIR"
+rm -rf "$ARTIFACT_DIR/ffi"
 
 
 # ====== CLI Executables
@@ -105,4 +127,7 @@ echo "Packing Confidential Identity CLI executables..."
 mkdir -p "$ARTIFACT_DIR/bin/linux-x64"
 cp "$ROOT/target/release/polymath-scp" "$ARTIFACT_DIR/bin/linux-x64/"
 cp "$ROOT/target/release/polymath-scv" "$ARTIFACT_DIR/bin/linux-x64/"
+tar -czvf confidential-identity-bin.tar.gz "$ARTIFACT_DIR/bin"
+mv confidential-identity-bin.tar.gz "$ROOT/$ARTIFACT_DIR"
+rm -rf "$ARTIFACT_DIR/bin"
 
