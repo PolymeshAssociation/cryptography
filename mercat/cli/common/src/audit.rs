78d6b491ded2ebdacfed4a293e9d0b2aacb85e00
Arash Afshar <arash.afshar.edu@gmail.com>
2021-04-11 11:05:50 -0600

    split to smaller functions

--- mercat/cli/common/src/audit.rs
+++ mercat/cli/common/src/audit.rs
@@ -3,8 +3,9 @@ use crate::{
     confidential_transaction_audit_result_file, confidential_transaction_file, construct_path,
     create_rng_from_seed, errors::Error, load_object, load_transaction_names, save_object,
     user_public_account_file, AuditResult, InitializedAssetTx, OrderedAssetInstruction,
-    OrderedPubAccount, TransferInstruction, TxNameIdInfo, AUDITOR_PUBLIC_ACCOUNT_FILE,
-    COMMON_OBJECTS_DIR, OFF_CHAIN_DIR, ON_CHAIN_DIR, SECRET_ACCOUNT_FILE,
+    OrderedPubAccount, TransferInstruction, TxAssetNameIdInfo, TxNameIdInfo, TxTransferNameIdInfo,
+    AUDITOR_PUBLIC_ACCOUNT_FILE, COMMON_OBJECTS_DIR, OFF_CHAIN_DIR, ON_CHAIN_DIR,
+    SECRET_ACCOUNT_FILE,
 };
 use codec::{Decode, Encode};
 use cryptography_core::asset_proofs::ElgamalSecretKey;
@@ -90,133 +91,147 @@ pub fn process_create_auditor(
     Ok(())
 }
 
+pub fn process_asset_audit(
+    tx_asset_info: TxAssetNameIdInfo,
+    auditor: &String,
+    tx_name: &String,
+    db_dir: PathBuf,
+) -> Result<(), Error> {
+    let instruction_path = asset_transaction_file(
+        tx_asset_info.tx_id,
+        &tx_asset_info.issuer,
+        AssetTxState::Initialization(TxSubstate::Started),
+    );
+    let instruction: OrderedAssetInstruction = load_object(
+        db_dir.clone(),
+        ON_CHAIN_DIR,
+        COMMON_OBJECTS_DIR,
+        &instruction_path,
+    )?;
+
+    let issuer_ordered_pub_account: OrderedPubAccount = load_object(
+        db_dir.clone(),
+        ON_CHAIN_DIR,
+        &tx_asset_info.issuer,
+        &user_public_account_file(&tx_asset_info.ticker),
+    )?;
+
+    let auditor_account: AuditorAccount =
+        load_object(db_dir.clone(), OFF_CHAIN_DIR, &auditor, SECRET_ACCOUNT_FILE)?;
+
+    let asset_tx = InitializedAssetTx::decode(&mut &instruction.data[..]).map_err(|error| {
+        Error::ObjectLoadError {
+            error,
+            path: construct_path(
+                db_dir.clone(),
+                ON_CHAIN_DIR,
+                COMMON_OBJECTS_DIR,
+                &instruction_path,
+            ),
+        }
+    })?;
+
+    let result = AssetAuditor {}.audit_asset_transaction(
+        &asset_tx,
+        &issuer_ordered_pub_account.pub_account,
+        &auditor_account,
+    );
+
+    let audit_result_path = asset_transaction_audit_result_file(
+        tx_asset_info.tx_id,
+        &tx_asset_info.issuer,
+        AssetTxState::Initialization(TxSubstate::Started),
+    );
+    let audit_result = AuditResult::from(&result);
+    save_object(
+        db_dir,
+        ON_CHAIN_DIR,
+        &auditor,
+        &audit_result_path,
+        &serde_json::to_string(&(tx_name, audit_result)).map_err(|_| Error::SerializeError)?,
+    )?;
+
+    result.map_err(|error| Error::LibraryError { error })
+}
+
+fn process_transfer_audit(
+    tx_transfer_info: TxTransferNameIdInfo,
+    auditor: &String,
+    tx_name: &String,
+    db_dir: PathBuf,
+) -> Result<(), Error> {
+    let instruction_path = confidential_transaction_file(
+        tx_transfer_info.tx_id,
+        &tx_transfer_info.sender,
+        TransferTxState::Justification(TxSubstate::Validated),
+    );
+    let instruction: TransferInstruction = load_object(
+        db_dir.clone(),
+        ON_CHAIN_DIR,
+        COMMON_OBJECTS_DIR,
+        &instruction_path,
+    )?;
+
+    let sender_ordered_pub_account: OrderedPubAccount = load_object(
+        db_dir.clone(),
+        ON_CHAIN_DIR,
+        &tx_transfer_info.sender,
+        &user_public_account_file(&tx_transfer_info.ticker),
+    )?;
+    let receiver_ordered_pub_account: OrderedPubAccount = load_object(
+        db_dir.clone(),
+        ON_CHAIN_DIR,
+        &tx_transfer_info.receiver,
+        &user_public_account_file(&tx_transfer_info.ticker),
+    )?;
+
+    let auditor_account: AuditorAccount =
+        load_object(db_dir.clone(), OFF_CHAIN_DIR, &auditor, SECRET_ACCOUNT_FILE)?;
+
+    let asset_tx = JustifiedTransferTx::decode(&mut &instruction.data[..]).map_err(|error| {
+        Error::ObjectLoadError {
+            error,
+            path: construct_path(
+                db_dir.clone(),
+                ON_CHAIN_DIR,
+                COMMON_OBJECTS_DIR,
+                &instruction_path,
+            ),
+        }
+    })?;
+
+    let result = CtxAuditor {}.audit_transaction(
+        &asset_tx,
+        &sender_ordered_pub_account.pub_account,
+        &receiver_ordered_pub_account.pub_account,
+        &auditor_account,
+    );
+
+    let audit_result_path = confidential_transaction_audit_result_file(
+        tx_transfer_info.tx_id,
+        &tx_transfer_info.sender,
+        TransferTxState::Justification(TxSubstate::Validated),
+    );
+    let audit_result = AuditResult::from(&result);
+    save_object(
+        db_dir,
+        ON_CHAIN_DIR,
+        &auditor,
+        &audit_result_path,
+        &serde_json::to_string(&(tx_name, audit_result)).map_err(|_| Error::SerializeError)?,
+    )?;
+
+    result.map_err(|error| Error::LibraryError { error })
+}
+
 pub fn process_audit(auditor: String, tx_name: String, db_dir: PathBuf) -> Result<(), Error> {
     let tx_info = load_transaction_names(db_dir.clone())[&tx_name].clone();
     match tx_info {
         TxNameIdInfo::Asset(tx_asset_info) => {
-            let instruction_path = asset_transaction_file(
-                tx_asset_info.tx_id,
-                &tx_asset_info.issuer,
-                AssetTxState::Initialization(TxSubstate::Started),
-            );
-            let instruction: OrderedAssetInstruction = load_object(
-                db_dir.clone(),
-                ON_CHAIN_DIR,
-                COMMON_OBJECTS_DIR,
-                &instruction_path,
-            )?;
-
-            let issuer_ordered_pub_account: OrderedPubAccount = load_object(
-                db_dir.clone(),
-                ON_CHAIN_DIR,
-                &tx_asset_info.issuer,
-                &user_public_account_file(&tx_asset_info.ticker),
-            )?;
-
-            let auditor_account: AuditorAccount =
-                load_object(db_dir.clone(), OFF_CHAIN_DIR, &auditor, SECRET_ACCOUNT_FILE)?;
-
-            let asset_tx =
-                InitializedAssetTx::decode(&mut &instruction.data[..]).map_err(|error| {
-                    Error::ObjectLoadError {
-                        error,
-                        path: construct_path(
-                            db_dir.clone(),
-                            ON_CHAIN_DIR,
-                            COMMON_OBJECTS_DIR,
-                            &instruction_path,
-                        ),
-                    }
-                })?;
-
-            let result = AssetAuditor {}.audit_asset_transaction(
-                &asset_tx,
-                &issuer_ordered_pub_account.pub_account,
-                &auditor_account,
-            );
-
-            let audit_result_path = asset_transaction_audit_result_file(
-                tx_asset_info.tx_id,
-                &tx_asset_info.issuer,
-                AssetTxState::Initialization(TxSubstate::Started),
-            );
-            let audit_result = AuditResult::from(&result);
-            save_object(
-                db_dir,
-                ON_CHAIN_DIR,
-                &auditor,
-                &audit_result_path,
-                &serde_json::to_string(&(tx_name, audit_result))
-                    .map_err(|_| Error::SerializeError)?,
-            )?;
-
-            result.map_err(|error| Error::LibraryError { error })
+            process_asset_audit(tx_asset_info, &auditor, &tx_name, db_dir.clone())
         }
         TxNameIdInfo::Transfer(tx_transfer_info) => {
-            let instruction_path = confidential_transaction_file(
-                tx_transfer_info.tx_id,
-                &tx_transfer_info.sender,
-                TransferTxState::Justification(TxSubstate::Validated),
-            );
-            let instruction: TransferInstruction = load_object(
-                db_dir.clone(),
-                ON_CHAIN_DIR,
-                COMMON_OBJECTS_DIR,
-                &instruction_path,
-            )?;
-
-            let sender_ordered_pub_account: OrderedPubAccount = load_object(
-                db_dir.clone(),
-                ON_CHAIN_DIR,
-                &tx_transfer_info.sender,
-                &user_public_account_file(&tx_transfer_info.ticker),
-            )?;
-            let receiver_ordered_pub_account: OrderedPubAccount = load_object(
-                db_dir.clone(),
-                ON_CHAIN_DIR,
-                &tx_transfer_info.receiver,
-                &user_public_account_file(&tx_transfer_info.ticker),
-            )?;
-
-            let auditor_account: AuditorAccount =
-                load_object(db_dir.clone(), OFF_CHAIN_DIR, &auditor, SECRET_ACCOUNT_FILE)?;
-
-            let asset_tx =
-                JustifiedTransferTx::decode(&mut &instruction.data[..]).map_err(|error| {
-                    Error::ObjectLoadError {
-                        error,
-                        path: construct_path(
-                            db_dir.clone(),
-                            ON_CHAIN_DIR,
-                            COMMON_OBJECTS_DIR,
-                            &instruction_path,
-                        ),
-                    }
-                })?;
-
-            let result = CtxAuditor {}.audit_transaction(
-                &asset_tx,
-                &sender_ordered_pub_account.pub_account,
-                &receiver_ordered_pub_account.pub_account,
-                &auditor_account,
-            );
-
-            let audit_result_path = confidential_transaction_audit_result_file(
-                tx_transfer_info.tx_id,
-                &tx_transfer_info.sender,
-                TransferTxState::Justification(TxSubstate::Validated),
-            );
-            let audit_result = AuditResult::from(&result);
-            save_object(
-                db_dir,
-                ON_CHAIN_DIR,
-                &auditor,
-                &audit_result_path,
-                &serde_json::to_string(&(tx_name, audit_result))
-                    .map_err(|_| Error::SerializeError)?,
-            )?;
-
-            result.map_err(|error| Error::LibraryError { error })
+            process_transfer_audit(tx_transfer_info, &auditor, &tx_name, db_dir.clone())
         }
     }
 }
