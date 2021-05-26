61fe7fa7dd6174523463a67416dd32d733e62829
Arash Afshar <arash.afshar.edu@gmail.com>
2021-05-26 11:31:24 -0600

    First draft of the api

--- confidential-identity-v2/src/lib.rs
+++ confidential-identity-v2/src/lib.rs
@@ -1,105 +1,168 @@
-//! The `claim_proofs` library contains API for generating
-//! claim proofs and verifying them as part of the
-//! Asset Granularity Unique Identity project.
+//! This library is the implementation of the 2nd iteration of the confidential identity system.
+//! The goal of this version is to enable users to create claim with minimal interaction with the
+//! PUIS and CDD providers. Moreover, in this version PUIS and CDD providers will store the least
+//! amount of information about a user which preserves the privacy of the users even further.
+//!
+//!
+//! The confidential identity libary v2 (CIL2) has the following actors.
+//! 1. User
+//! 2. Issuer: The PUIS system
+//! 3. Identity verifier: The CDD Provider
+//! 4. Claim verifier: PolyMesh chain
+//!
+//!
+//! The CIL2 has three main phases:
+//! 1. The user communicates with an external identity verifier and obtains a
+//!    fingerprint. This is done outside of this library.
+//! 2. The user creates a private/public key pair using the APIs provided by the is library and
+//!    registers the public key with the issuer along with the fingerprint.
+//! 2. [TODO: this should probably be merged into the above] The user then sends the fingerprint
+//!    to the issuer and obtains a certificate, which is an attestation of the the identity of the
+//!    user. This library provides an API for creating this certificate. The following apis are
+//!    related to this phase:
+//!    
+//!    - TODO: list the APIs
+//! 3. The uer creates a CDD Claim and submits it to the chain for verification.
+//!    This library provides an API for creating this certificate. The following apis are
+//!    related to this phase:
+//!    
+//!    - TODO: list the APIs
+//! 4. The user creates a SCOPE claim and submits it to the chain for verification.
+//!    This library provides an API for creating this certificate. The following apis are
+//!    related to this phase:
+//!    
+//!    - TODO: list the APIs
+//!
+//! Example workflow:
+//!
+//! ```
+//! use confidential_identity_v2::{UserKeys, IdentitySignature, CddClaim, ScopeClaim};
+//! use rand::{thread_rng, Rng};
+//!
+//! // ---------------- Done by the user
+//! let mut rng = thread_rng();
+//! let keys = UserKeys::new(&mut rng);
+//! let identity_proof = keys.generate_identity_proof();
+//! // Send `identity_proof` to issuer (PUIS)
+//!
+//! // ---------------- Done by issuer (PUIS)
+//! let verified_identity = identity_proof.verify().expect("Identity verification must pass");
+//! let identity_signature = IdentitySignature::new(verified_identity);
+//! // Send the `identity_signature` back to the user.
+//!
+//! // ---------------- Done by the user
+//! let cdd_claim = CddClaim::new(&identity_signature);
+//! // send `cdd_claim` to PolMesh
+//!
+//! // ---------------- Done by the PolyMesh
+//! cdd_claim.verify().expect("CDD claim verification must pass");
+//!
+//! // ---------------- Done by the user
+//! let scope_claim = ScopeClaim::new(&identity_signature, &cdd_claim);
+//! // send `scope_claim` to PolyMesh
+//!
+//! // ---------------- Done by the PolyMesh
+//! scope_claim.verify().expect("SCOPE claim verification must pass");
+//! ```
 
-#![cfg_attr(not(feature = "std"), no_std)]
-
-#[cfg(not(feature = "std"))]
-#[macro_use]
-extern crate alloc;
-
-pub use claim_proofs::{CddClaimData, CddId, ScopeClaimData, ScopeClaimProof, ScopeClaimProofData};
-pub use cryptography_core;
-pub use curve25519_dalek::{
-    self,
-    ristretto::{CompressedRistretto, RistrettoPoint},
-    scalar::Scalar,
+use cryptography_core::{
+    asset_proofs::{ElgamalPublicKey, ElgamalSecretKey},
+    Scalar,
 };
-use errors::Fallible;
 use rand_core::{CryptoRng, RngCore};
 
-pub fn random_claim<R: RngCore + CryptoRng>(rng: &mut R) -> (CddClaimData, ScopeClaimData) {
-    let investor_unique_id = Scalar::random(rng);
-    (
-        CddClaimData {
-            investor_did: Scalar::random(rng),
-            investor_unique_id,
-        },
-        ScopeClaimData {
-            scope_did: Scalar::random(rng),
-            investor_unique_id,
-        },
-    )
+/// Hook desc.
+///
+/// Detailed aoesuthaoesh `arg1` aontseuhonsetahus `arg2`. `out1`
+///
+/// # Arguments
+/// If not redundant
+///
+/// # Examples
+/// executable examples, edge cases, etc
+///
+/// # Errors
+/// * if there is any or they can be added to desc
+///
+/// # Panics
+/// * if there is any
+
+pub struct UserKeys {
+    public: ElgamalPublicKey,
+    secret: ElgamalSecretKey,
 }
 
-/// That `ensure` does not transform into a string representation like `failure::ensure` is doing.
-#[allow(unused_macros)]
-macro_rules! ensure {
-    ($predicate:expr, $context_selector:expr) => {
-        if !$predicate {
-            return Err($context_selector.into());
+#[derive(Clone)]
+pub struct IdentityZkClaim {}
+
+pub struct IdentityZkVerifiedClaim {}
+
+pub struct IdentityZkProof {
+    claim: IdentityZkClaim,
+}
+
+pub struct IdentitySignature {}
+
+pub struct CddClaim {}
+
+pub struct ScopeClaim {}
+
+impl UserKeys {
+    pub fn new<R: RngCore + CryptoRng>(rng: &mut R) -> Self {
+        let key = Scalar::random(rng);
+        let secret = ElgamalSecretKey::new(key);
+        let public = secret.get_public_key();
+        Self { secret, public }
+    }
+    pub fn generate_identity_proof(&self) -> IdentityZkProof {
+        // generate zkp prove
+        IdentityZkProof::new()
+    }
+}
+
+impl IdentityZkProof {
+    pub fn new() -> Self {
+        // Create the non-interactive proof
+        Self {
+            claim: IdentityZkClaim {},
         }
-    };
+    }
+
+    pub fn verify(&self) -> Result<IdentityZkVerifiedClaim, ()> {
+        // Verify the proov
+        Ok(IdentityZkVerifiedClaim::from(self.claim.clone()))
+    }
 }
 
-/// Helper macro to assert that `predicate` is an `Error::from( $err)`.
-#[allow(unused_macros)]
-macro_rules! assert_err {
-    ($predicate:expr, $err:expr) => {
-        assert_eq!($predicate.expect_err("Error expected").kind(), &$err);
-    };
+impl From<IdentityZkClaim> for IdentityZkVerifiedClaim {
+    fn from(claim: IdentityZkClaim) -> Self {
+        Self {}
+    }
 }
 
-pub trait ProviderTrait {
-    /// Compute the CDD_ID. \
-    /// CDD_ID = PedersenCommitment(INVESTOR_DID, INVESTOR_UNIQUE_ID, [INVESTOR_DID | INVESTOR_UNIQUE_ID])
-    ///
-    /// # Arguments
-    /// * `cdd_claim` is the CDD claim from which to generate the CDD_ID
-    ///
-    /// # Output
-    /// * The Pedersen commitment result.
-    fn create_cdd_id(cdd_claim: &CddClaimData) -> CddId;
+impl IdentitySignature {
+    pub fn new(verified_identity: IdentityZkVerifiedClaim) -> Self {
+        // Creates a signature for a verified identity
+        Self {}
+    }
 }
 
-pub trait InvestorTrait {
-    /// Creates a SCOPE_ID and then generates two proofs: \
-    /// 1. Prove that the scope id has the following form: INVESTOR_UNIQUE_ID * Hash(SCOPE_DID) \
-    /// 2. Prove that the cdd_id and the scope_id share the same INVESTOR_UNIQUE_ID
-    ///
-    /// # Arguments
-    /// * `cdd_claim` is the CDD claim from which the CDD_ID was generated.
-    /// * `scope_claim` is the Scope claim from which the SCOPE_ID will be generated.
-    ///
-    /// # Output
-    /// * The proofs.
-    fn create_scope_claim_proof<R: RngCore + CryptoRng>(
-        cdd_claim: &CddClaimData,
-        scope_claim: &ScopeClaimData,
-        rng: &mut R,
-    ) -> ScopeClaimProof;
+impl CddClaim {
+    pub fn new(identity_signature: &IdentitySignature) -> Self {
+        Self {}
+    }
+    pub fn verify(&self) -> Result<(), ()> {
+        Ok(())
+    }
 }
 
-pub trait VerifierTrait {
-    /// Verifies the two proofs and if any of them fail, returns an error.
-    ///
-    /// # Arguments
-    /// * `proof`: the proofs obtained from the call to `create_scope_claim_proof`.
-    /// * `investor_did`: the INVESTOR_DID.
-    /// * `scope_did`: is the SCOPE_DID.
-    /// * `cdd_id`: the CDD_ID.
-    ///
-    /// # Errors
-    /// * `ZkpError`: If the scope id and cdd id do not match.
-    /// * `SignatureError`: If the scope id is not wellformed.
-    fn verify_scope_claim_proof(
-        proof: &ScopeClaimProof,
-        investor_did: &Scalar,
-        scope_did: &Scalar,
-        cdd_id: &CddId,
-    ) -> Fallible<()>;
+impl ScopeClaim {
+    pub fn new(identity_signature: &IdentitySignature, cdd_claim: &CddClaim) -> Self {
+        Self {}
+    }
+    pub fn verify(&self) -> Result<(), ()> {
+        Ok(())
+    }
 }
 
-pub mod claim_proofs;
 pub mod errors;
-pub mod sign;
