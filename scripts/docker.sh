1943b872849c04ca4967e0e31cd53c24ee485f4f
Arash Afshar <Arash-Afshar@users.noreply.github.com>
2021-05-26 08:30:29 -0600

    Update CI (#140)
    * Update the CI to use a new image
    * Create a test script to run all the tests

---
+++ scripts/docker.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+set -eo pipefail
+
+ROOT=$( cd `dirname $0`/..;  pwd )
+
+
+if [ "$#" -eq 0 ]; then
+    echo "$0 run/build/shell"
+    exit 1
+fi
+
+if [ "$1" == "shell" ] 
+then
+  docker run --rm -it -v "$(pwd)":/src cryptography /bin/bash
+fi
+
+if [ "$1" == "build" ] 
+then
+  cd docker && docker build -t cryptography .
+fi
+
+if [ "$1" == "run" ] 
+then
+  shift 1
+  docker run -v "$(pwd)":/src -w /src cryptography $@
+fi
