c1f2785fbd38c0fa5aa0f610cb0d45f314130544
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2022-06-13 23:12:31 +0800

    Remove use of 'failure' crate.

--- confidential-identity/src/errors.rs
+++ confidential-identity/src/errors.rs
@@ -1,16 +1,14 @@
-use failure::{Backtrace, Context, Fail};
-
 use sp_std::{fmt, result::Result};
 
 #[derive(Debug)]
 pub struct Error {
-    inner: Context<ErrorKind>,
+    kind: ErrorKind,
 }
 
 impl Error {
     #[inline]
     pub fn kind(&self) -> &ErrorKind {
-        self.inner.get_context()
+        &self.kind
     }
 }
 
@@ -18,43 +16,31 @@ impl From<ErrorKind> for Error {
     #[inline]
     fn from(kind: ErrorKind) -> Error {
         Error {
-            inner: Context::new(kind),
+            kind,
         }
-    }
-}
-
-impl From<Context<ErrorKind>> for Error {
-    #[inline]
-    fn from(inner: Context<ErrorKind>) -> Error {
-        Error { inner }
-    }
-}
-
-impl Fail for Error {
-    fn cause(&self) -> Option<&dyn Fail> {
-        self.inner.cause()
-    }
-
-    #[inline]
-    fn backtrace(&self) -> Option<&Backtrace> {
-        self.inner.backtrace()
     }
 }
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        fmt::Display::fmt(&self.inner, f)
+        use ErrorKind::*;
+        match &self.kind {
+            ZkpError => {
+                write!(f, "Scope claim does not match cdd claim: Zero Knowledge Proof failed.")
+            },
+            SignatureError => {
+                write!(f, "Scope id is not wellformed: signature verification failed.")
+            },
+        }
     }
 }
 
-#[derive(Fail, Clone, Debug, Eq, PartialEq)]
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub enum ErrorKind {
     /// Scope claim does not match cdd claim: Zero Knowledge Proof failed.
-    #[fail(display = "Scope claim does not match cdd claim: Zero Knowledge Proof failed.")]
     ZkpError,
 
     /// Scope id is not wellformed: signature verification failed.
-    #[fail(display = "Scope id is not wellformed: signature verification failed.")]
     SignatureError,
 }
 
